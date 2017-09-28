966714270f9163cffe85d5e5a7a409c966330e4a
Mark Hadley <hadleym@email.arizona.edu>
2017-09-28 15:06:21 -0600

    Added setup.py

---
+++ watermark/wrappers/python/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='python3-cxs',
+        version='0.0.1',
+        packages=['cxs'],
+        author='Mark Hadley',
+        author_email='mark.hadley@evernym.com',
+        description='cxs project',
+        test_requires=['pytest']
+)
+
+
