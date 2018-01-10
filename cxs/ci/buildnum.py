3cb6a831450bc79e5b36d79b6caf9290e4c3621d
Mark Hadley <hadleym@email.arizona.edu>
2018-01-09 17:44:39 -0700

    Cargo command 'cargo libcxs-debian' works

---
+++ cxs/ci/buildnum.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import json
+import time
+
+
+TIME_OFF_SET = 1515500000
+
+
+def main():
+    return int(time.time() - TIME_OFF_SET)
+
+
+if __name__ == '__main__':
+    main()
+
