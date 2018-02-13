89770a00af1adb60b12875e19f437db0f3cb289c
Mark Hadley <mark.hadley@evernym.com>
2018-02-13 16:50:22 -0700

    Modifying Jenkinsfile

--- cxs/ci/create_debian.sh
+++ cxs/ci/create_debian.sh
@@ -13,7 +13,7 @@ pushd cxs/wrappers/node/ci
 # build Docker-debian
 popd
 echo "Building Docker"
-#docker build -t create-debian -f cxs/ci/Dockerfile-create-debian .
+docker build -t create-debian -f cxs/ci/Dockerfile-create-debian .
 
 echo "Current directory:"
 echo $(pwd)
@@ -27,7 +27,7 @@ ls cxs
 echo "runing git log command:"
 git log
 echo "Running Docker"
-#docker run --rm -v ${VOLUME}:/output -v $SDK:/sdk create-debian
+docker run --rm -v ${VOLUME}:/output -v $SDK:/sdk create-debian
 
 
 
