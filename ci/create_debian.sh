876a790226a737d34b1c29b38158ebd44eb71eb7
Mark Hadley <mark.hadley@evernym.com>
2018-02-14 09:28:17 -0700

    modify create_debian.sh

---
+++ ci/create_debian.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VOLUME="artifact1"
+CXS=`readlink -f cxs`
+
+# clean the artifact volume
+docker volume rm $VOLUME
+
+# make the volume from sdk
+pushd cxs/wrappers/node/ci
+./create_volume.sh ~/cxs $CXS
+
+# build Docker-debian
+popd
+echo "Building Docker"
+docker build -t create-debian -f cxs/ci/Dockerfile-create-debian .
+
+echo "############################"
+echo "Directory Listing for Current Directory"
+ls .
+echo "############################"
+echo "directory listing for ${CXS}:"
+ls $CXS
+
+echo "Running Docker"
+docker run --rm -v ${VOLUME}:/output -v $CXS:/cxs create-debian
+
