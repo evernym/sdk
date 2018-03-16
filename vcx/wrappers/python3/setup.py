aa4bc56d8499ff72836a4889023d58d3662b5258
Mark Hadley <mark.hadley@evernym.com>
2018-03-16 09:25:53 -0600

    Fixed Setup bug for install

--- vcx/wrappers/python3/setup.py
+++ vcx/wrappers/python3/setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, find_packages
 import os
+VERSION = 'VCX_VERSION'
+
+
+def get_version():
+    if hasattr(os.environ, VERSION):
+        return os.environ[VERSION]
+    else:
+        return '0.1'
+
+
 setup(
     name='vcx',
-    version=os.environ['VCX_VERSION'],
+    version=get_version(),
     description='Wrapper for libcxs',
     long_description='None...for now',
     author='Devin Fisher, Ryan Marsh, Mark Hadley, Doug Wightman',
