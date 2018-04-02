40dbefb909f4ae45233a798fb3bb85aa856b698e
Douglas Wightman <douglas.wightman@evernym.com>
2018-04-02 08:42:44 -0600

    implement anon-decrypt for testing

--- vcx/libvcx/src/utils/libindy/crypto.rs
+++ vcx/libvcx/src/utils/libindy/crypto.rs
@@ -32,6 +32,13 @@ extern {
                       encrypted_len: u32,
                       cb: Option<extern fn(command_handle_: i32, err: i32, sender_vk: *const c_char, msg_data: *const u8, msg_len: u32)>) -> i32;
 
+    fn indy_crypto_anon_decrypt(command_handle: i32,
+                                wallet_handle: i32,
+                                my_vk: *const c_char,
+                                encrypted_msg: *const u8,
+                                encrypted_len: u32,
+                                cb: Option<extern fn(command_handle_: i32, err: i32, msg_data: *const u8, msg_len: u32)>) -> i32;
+
     fn indy_crypto_sign(command_handle: i32,
                  wallet_handle: i32,
                  my_vk: *const c_char,
@@ -113,6 +120,27 @@ pub fn parse_msg(wallet_handle: i32, recipient_vk: &str, msg: &[u8]) -> Result<V
         Ok(msg)
 }
 
+pub fn parse_anonymous_msg(wallet_handle: i32, recipient_vk: &str, msg: &[u8]) -> Result<Vec<u8>, u32> {
+    if settings::test_indy_mode_enabled() { return Ok(Vec::from(msg).to_owned()) }
+
+    debug!("parse_msg vk: {}",recipient_vk);
+
+    let rtn_obj = Return_I32_BIN::new()?;
+    let recipient_vk = CString::new(recipient_vk).map_err(map_string_error)?;
+
+    unsafe {
+            indy_function_eval(
+                indy_crypto_anon_decrypt(rtn_obj.command_handle,
+                                   wallet_handle,
+                                  recipient_vk.as_ptr(),
+                                  msg.as_ptr() as *const u8,
+                                  msg.len() as u32,
+                                  Some(rtn_obj.get_callback()))
+            ).map_err(map_indy_error_code)?;
+    }
+
+    rtn_obj.receive(TimeoutUtils::some_long())
+}
 
 pub fn sign(wallet_handle: i32, my_vk: &str, msg: &[u8]) -> Result<Vec<u8>, u32> {
     if settings::test_indy_mode_enabled() {return Ok(Vec::from(msg).to_owned())}
@@ -155,25 +183,45 @@ pub mod tests {
     use utils::constants::*;
 
     #[test]
-    fn test_send_msg() {
+    fn test_encrypt_decrypt() {
         settings::set_defaults();
         settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE,"false");
-        let my_wallet = wallet::init_wallet("test_send_msg_my_wallet").unwrap();
-        let their_wallet = wallet::init_wallet("test_send_msg_their_wallet").unwrap();
+        let my_wallet = wallet::init_wallet("test_encrypt_decrypt").unwrap();
 
         let (my_did, my_vk) = SignusUtils::create_and_store_my_did(my_wallet, Some(MY1_SEED)).unwrap();
-        let (their_did, their_vk) = SignusUtils::create_and_store_my_did(their_wallet, Some(MY2_SEED)).unwrap();
+        let (their_did, their_vk) = SignusUtils::create_and_store_my_did(my_wallet, Some(MY2_SEED)).unwrap();
 
         SignusUtils::store_their_did_from_parts(my_wallet, their_did.as_ref(), their_vk.as_ref()).unwrap();
-        SignusUtils::store_their_did_from_parts(their_wallet, my_did.as_ref(), my_vk.as_ref()).unwrap();
+        SignusUtils::store_their_did_from_parts(my_wallet, my_did.as_ref(), my_vk.as_ref()).unwrap();
 
         let message = "this is a test message for encryption";
         let encrypted_message = prep_msg(my_wallet, my_vk.as_ref(), their_vk.as_ref(),message.as_bytes()).unwrap();
-        let decrypted_message = parse_msg(their_wallet,their_vk.as_ref(),&encrypted_message[..]).unwrap();
+        let decrypted_message = parse_msg(my_wallet,their_vk.as_ref(),&encrypted_message[..]).unwrap();
 
         assert_eq!(message.as_bytes().to_vec(), decrypted_message);
-        wallet::delete_wallet("test_send_msg_my_wallet").unwrap();
-        wallet::delete_wallet("test_send_msg_their_wallet").unwrap();
+        wallet::delete_wallet("test_encrypt_decrypt").unwrap();
+    }
+
+    #[test]
+    fn test_anon_encrypt_decrypt() {
+        settings::set_defaults();
+        settings::set_config_value(settings::CONFIG_ENABLE_TEST_MODE,"false");
+        let my_wallet = wallet::init_wallet("test_anon_encrypt_decrypt").unwrap();
+
+        let (my_did, my_vk) = SignusUtils::create_and_store_my_did(my_wallet, Some(MY1_SEED)).unwrap();
+        let (their_did, their_vk) = SignusUtils::create_and_store_my_did(my_wallet, Some(MY2_SEED)).unwrap();
+
+        SignusUtils::store_their_did_from_parts(my_wallet, their_did.as_ref(), their_vk.as_ref()).unwrap();
+        SignusUtils::store_their_did_from_parts(my_wallet, my_did.as_ref(), my_vk.as_ref()).unwrap();
+
+        let message = "this is a test message for encryption";
+        let encrypted_message = prep_anonymous_msg(their_vk.as_ref(), message.as_bytes()).unwrap();
+        let decrypted_message = parse_anonymous_msg(my_wallet,their_vk.as_ref(),&encrypted_message[..]).unwrap();
+
+        assert_eq!(message.as_bytes().to_vec(), decrypted_message);
+        println!("{:?}", message.as_bytes().to_vec());
+        println!("{:?}", decrypted_message);
+        wallet::delete_wallet("test_anon_encrypt_decrypt").unwrap();
     }
 }
 
