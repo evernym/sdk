811f6656314697ea6f15f503e9724a6bb060d04e
Ryan Marsh <ryan.marsh44@gmail.com>
2018-04-10 10:43:55 -0600

    upadated failing proof tests

--- vcx/libvcx/src/messages/proofs/proof_request.rs
+++ vcx/libvcx/src/messages/proofs/proof_request.rs
@@ -25,15 +25,6 @@ struct ProofTopic {
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-pub struct OldAttr {
-    pub name: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub schema_seq_no: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub issuer_did: Option<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct AttrInfo {
     pub name: String,
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -53,18 +44,6 @@ pub struct SchemaKeyFilter {
     pub did: Option<String> //Schema DID
 }
 
-//Todo: Move Predicate to a common place for both proof_req and proof msg
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-pub struct OldPredicate {
-    pub attr_name: String,
-    pub p_type: String,
-    pub value: i32,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub schema_seq_no: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub issuer_did: Option<String>,
-}
-
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct PredicateInfo {
     pub attr_name: String,
@@ -74,18 +53,11 @@ pub struct PredicateInfo {
     pub restrictions: Option<Vec<Filter>>,
 }
 
-// Todo: Remove -> no longer needed with libindy format
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-pub struct ProofAttrs {
-    attrs: Vec<AttrInfo>
-}
-
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct ProofPredicates {
     predicates: Vec<PredicateInfo>
 }
 
-// TODO: update format to new libindy
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct ProofRequestData{
     nonce: String,
@@ -106,14 +78,6 @@ pub struct ProofRequestMessage{
     #[serde(skip_serializing, default)]
     validate_rc: u32,
     pub msg_ref_id: Option<String>,
-}
-
-impl ProofAttrs {
-    pub fn create() -> ProofAttrs {
-        ProofAttrs {
-            attrs: Vec::new()
-        }
-    }
 }
 
 impl ProofPredicates {
@@ -257,8 +221,8 @@ mod tests {
     use super::*;
     use messages::{proof_request};
 
-    static REQUESTED_ATTRS: &'static str = r#"[{"name":"person name"},{"schema_seq_no":1,"name":"address_1"},{"schema_seq_no":2,"issuer_did":"8XFh8yBzrpJQmNyZzgoTqB","name":"address_2"},{"schema_seq_no":1,"name":"city"},{"schema_seq_no":1,"name":"state"},{"schema_seq_no":1,"name":"zip"}]"#;
-    static REQUESTED_PREDICATES: &'static str = r#"[{"attr_name":"age","p_type":"GE","value":18,"schema_seq_no":1,"issuer_did":"DID1"}]"#;
+    static REQUESTED_ATTRS: &'static str = r#"[ { "name":"age", "restrictions":[ { "issuer_did":"8XFh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"Faber Student Info", "version":"1.0", "did":"6XFh8yBzrpJQmNyZzgoTqB" } }, { "issuer_did":"66Fh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"BYU Student Info", "version":"1.0", "did":"5XFh8yBzrpJQmNyZzgoTqB" } } ] }, { "name":"name", "restrictions":[ { "issuer_did":"77Fh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"Faber Student Info", "version":"1.0", "did":"6XFh8yBzrpJQmNyZzgoTqB" } }, { "issuer_did":"66Fh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"BYU Student Info", "version":"1.0", "did":"5XFh8yBzrpJQmNyZzgoTqB" } } ] } ]"#;
+    static REQUESTED_PREDICATES: &'static str = r#"[ { "attr_name":"age", "p_type":"GE", "value":22, "restrictions":[ { "issuer_did":"8XFh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"Faber Student Info", "version":"1.0", "did":"6XFh8yBzrpJQmNyZzgoTqB" } }, { "issuer_did":"66Fh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"BYU Student Info", "version":"1.0", "did":"5XFh8yBzrpJQmNyZzgoTqB" } } ] } ]"#;
 
     #[test]
     fn test_create_proof_request_data() {
@@ -295,141 +259,33 @@ mod tests {
             .requested_predicates(REQUESTED_PREDICATES)
             .clone();
 
-        let proof_request_test: serde_json::Value = json!({
-            "@type": { "name": "PROOF_REQUEST", "version": "1.3" },
-            "@topic": { "tid": 89, "mid": 98 },
-            "proof_request_data": {
-                "nonce": "123432421212",
-                "name": "Test",
-                "version": "3.75",
-                "requested_attrs": {
-                    "person name_0": { "name": "person name" },
-                    "address_1_1": { "schema_seq_no": 1, "name": "address_1" },
-                    "address_2_2": {
-                        "schema_seq_no": 2,
-                        "issuer_did": "8XFh8yBzrpJQmNyZzgoTqB",
-                        "name": "address_2",
-                    },
-                    "city_3": {
-                        "schema_seq_no": 1,
-                        "name": "city",
-                    },
-                    "state_4": {
-                        "schema_seq_no": 1,
-                        "name": "state",
-                    },
-                    "zip_5": {
-                        "schema_seq_no": 1,
-                        "name": "zip",
-                    },
-                },
-                "requested_predicates": {
-                        "age_0": {
-                            "schema_seq_no": 2,
-                            "issuer_did": "8XFh8yBzrpJQmNyZzgoTqB",
-                            "attr_name": "age",
-                            "p_type": "GE",
-                            "value": 18
-                        }
-                },
-            },
-        });
         let serialized_msg = request.serialize_message().unwrap();
         assert!(serialized_msg.contains(r#""@type":{"name":"PROOF_REQUEST","version":"1.3"}"#));
         assert!(serialized_msg.contains(r#"@topic":{"mid":98,"tid":89}"#));
         assert!(serialized_msg.contains(r#"proof_request_data":{"nonce":"123432421212","name":"Test","version":"3.75","requested_attrs""#));
-        assert!(serialized_msg.contains(r#""zip_5":{"name":"zip","schema_seq_no":1}"#));
-        assert!(serialized_msg.contains(r#""age_0":{"attr_name":"age","p_type":"GE","value":18,"schema_seq_no":1,"issuer_did":"DID1"}"#));
+        assert!(serialized_msg.contains(r#""age_1":{"name":"age","restrictions":[{"issuer_did":"8XFh8yBzrpJQmNyZzgoTqB","schema_key":{"name":"Faber Student Info","version":"1.0","did":"6XFh8yBzrpJQmNyZzgoTqB"}},{"issuer_did":"66Fh8yBzrpJQmNyZzgoTqB","schema_key":{"name":"BYU Student Info","version":"1.0","did":"5XFh8yBzrpJQmNyZzgoTqB"}}"#));
+        assert!(serialized_msg.contains(r#"age_1":{"attr_name":"age","p_type":"GE","value":22,"restrictions":[{"issuer_did":"8XFh8yBzrpJQmNyZzgoTqB","schema_key":{"name":"Faber Student Info","version":"1.0","did":"6XFh8yBzrpJQmNyZzgoTqB"}},{"issuer_did":"66Fh8yBzrpJQmNyZzgoTqB","schema_key":{"name":"BYU Student Info","version":"1.0","did":"5XFh8yBzrpJQmNyZzgoTqB"}}"#));
     }
 
     #[test]
     fn test_requested_attrs_constructed_correctly() {
-        let new_req_attrs = r#"[
-                                       {
-                                          "name":"age",
-                                          "restrictions":[
-                                             {
-                                                "issuer_did":"8XFh8yBzrpJQmNyZzgoTqB",
-                                                "schema_key":{
-                                                   "name":"Faber Student Info",
-                                                   "version":"1.0",
-                                                   "did":"6XFh8yBzrpJQmNyZzgoTqB"
-                                                }
-                                             },
-                                             {
-                                                "issuer_did":"66Fh8yBzrpJQmNyZzgoTqB",
-                                                "schema_key":{
-                                                   "name":"BYU Student Info",
-                                                   "version":"1.0",
-                                                   "did":"5XFh8yBzrpJQmNyZzgoTqB"
-                                                }
-                                             }
-                                          ]
-                                       },
-                                       {
-                                          "name":"name",
-                                          "restrictions":[
-                                             {
-                                                "issuer_did":"77Fh8yBzrpJQmNyZzgoTqB",
-                                                "schema_key":{
-                                                   "name":"Faber Student Info",
-                                                   "version":"1.0",
-                                                   "did":"6XFh8yBzrpJQmNyZzgoTqB"
-                                                }
-                                             },
-                                             {
-                                                "issuer_did":"66Fh8yBzrpJQmNyZzgoTqB",
-                                                "schema_key":{
-                                                   "name":"BYU Student Info",
-                                                   "version":"1.0",
-                                                   "did":"5XFh8yBzrpJQmNyZzgoTqB"
-                                                }
-                                             }
-                                          ]
-                                       }
-                                    ]"#;
         let mut check_req_attrs: HashMap<String, AttrInfo> = HashMap::new();
         let attr_info1: AttrInfo = serde_json::from_str(r#"{ "name":"age", "restrictions":[ { "issuer_did":"8XFh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"Faber Student Info", "version":"1.0", "did":"6XFh8yBzrpJQmNyZzgoTqB" } }, { "issuer_did":"66Fh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"BYU Student Info", "version":"1.0", "did":"5XFh8yBzrpJQmNyZzgoTqB" } } ] }"#).unwrap();
         let attr_info2: AttrInfo = serde_json::from_str(r#"{ "name":"name", "restrictions":[ { "issuer_did":"77Fh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"Faber Student Info", "version":"1.0", "did":"6XFh8yBzrpJQmNyZzgoTqB" } }, { "issuer_did":"66Fh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"BYU Student Info", "version":"1.0", "did":"5XFh8yBzrpJQmNyZzgoTqB" } } ] }"#).unwrap();
         check_req_attrs.insert("age_1".to_string(), attr_info1);
         check_req_attrs.insert("name_2".to_string(), attr_info2);
 
-        let mut request = proof_request().requested_attrs(new_req_attrs).clone();
+        let mut request = proof_request().requested_attrs(REQUESTED_ATTRS).clone();
         assert_eq!(request.proof_request_data.requested_attrs, check_req_attrs);
     }
 
     #[test]
     fn test_requested_predicates_constructed_correctly() {
-        let new_predicates = r#"[
-                                       {
-                                          "attr_name":"age",
-                                          "p_type":"GE",
-                                          "value":22,
-                                          "restrictions":[
-                                             {
-                                                "issuer_did":"8XFh8yBzrpJQmNyZzgoTqB",
-                                                "schema_key":{
-                                                   "name":"Faber Student Info",
-                                                   "version":"1.0",
-                                                   "did":"6XFh8yBzrpJQmNyZzgoTqB"
-                                                }
-                                             },
-                                             {
-                                                "issuer_did":"66Fh8yBzrpJQmNyZzgoTqB",
-                                                "schema_key":{
-                                                   "name":"BYU Student Info",
-                                                   "version":"1.0",
-                                                   "did":"5XFh8yBzrpJQmNyZzgoTqB"
-                                                }
-                                             }
-                                          ]
-                                       }
-                                    ]"#;
         let mut check_predicates: HashMap<String, PredicateInfo> = HashMap::new();
         let attr_info1: PredicateInfo = serde_json::from_str(r#"{ "attr_name":"age","p_type":"GE","value":22, "restrictions":[ { "issuer_did":"8XFh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"Faber Student Info", "version":"1.0", "did":"6XFh8yBzrpJQmNyZzgoTqB" } }, { "issuer_did":"66Fh8yBzrpJQmNyZzgoTqB", "schema_key":{ "name":"BYU Student Info", "version":"1.0", "did":"5XFh8yBzrpJQmNyZzgoTqB" } } ] }"#).unwrap();
         check_predicates.insert("age_1".to_string(), attr_info1);
 
-        let mut request = proof_request().requested_predicates(new_predicates).clone();
+        let mut request = proof_request().requested_predicates(REQUESTED_PREDICATES).clone();
         assert_eq!(request.proof_request_data.requested_predicates, check_predicates);
     }
 }
