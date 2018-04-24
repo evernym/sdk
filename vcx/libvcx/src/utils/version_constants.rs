8e20506bdd3dc48f656bbac0a1035abcca72f5e2
Douglas Wightman <douglas.wightman@evernym.com>
2018-04-24 10:32:48 -0600

    EN-549 - enable "vcx_version" to dynamically extract library version

--- vcx/libvcx/src/utils/version_constants.rs
+++ vcx/libvcx/src/utils/version_constants.rs
@@ -1,2 +1,8 @@
+use std::ffi::CString;
+
 pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 pub const REVISION: &'static str = "+338ad01";
+
+lazy_static!{
+    pub static ref VERSION_STRING: CString = CString::new(format!("{}{}", VERSION, REVISION)).unwrap();
+}
