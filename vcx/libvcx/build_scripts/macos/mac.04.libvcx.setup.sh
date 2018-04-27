e3fcb367c6b8f84c881ac3b09df05d0223a54d7c
Norman Jarvis <norman.jarvis@evernym.com>
2018-04-27 14:07:46 -0600

    [CO-1313] - Added the -f flag on the ln command so that the soft link will be removed and restablished

--- vcx/libvcx/build_scripts/macos/mac.04.libvcx.setup.sh
+++ vcx/libvcx/build_scripts/macos/mac.04.libvcx.setup.sh
@@ -5,18 +5,12 @@ LIBINDY_HEADER_PATH=$PWD/vcx-indy-sdk/libindy/include
 VCXHEADER_PATH=$PWD/include/vcx.h
 
 ls -al $LIBINDY_PATH
-if [ ! -f /usr/local/lib/libindy.dylib ]; then
-    ln -s $LIBINDY_PATH /usr/local/lib/libindy.dylib
-fi
+ln -sf $LIBINDY_PATH /usr/local/lib/libindy.dylib
 otool -L /usr/local/lib/libindy.dylib
 
-if [ ! -f /usr/local/include/vcx.h ]; then
-    ln -s $VCXHEADER_PATH /usr/local/include/vcx.h
-fi
+ln -sf $VCXHEADER_PATH /usr/local/include/vcx.h
 
 for h in `ls $LIBINDY_HEADER_PATH`
 do
-    if [ ! -f /usr/local/include/$h ]; then
-        ln -s $LIBINDY_HEADER_PATH/$h /usr/local/include/$h
-    fi
+    ln -sf $LIBINDY_HEADER_PATH/$h /usr/local/include/$h
 done
