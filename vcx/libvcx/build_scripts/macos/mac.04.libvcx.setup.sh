9bd5196d552fcc1819c04525e5ffe7bbf2916ff8
Norman Jarvis <norman.jarvis@evernym.com>
2018-04-27 13:51:58 -0600

    [CO-1313] - Moving the macos build scripts to a folder called build_scripts/macos

--- vcx/libvcx/build_scripts/macos/mac.04.libvcx.setup.sh
+++ vcx/libvcx/build_scripts/macos/mac.04.libvcx.setup.sh
@@ -5,12 +5,18 @@ LIBINDY_HEADER_PATH=$PWD/vcx-indy-sdk/libindy/include
 VCXHEADER_PATH=$PWD/include/vcx.h
 
 ls -al $LIBINDY_PATH
-ln -sf $LIBINDY_PATH /usr/local/lib/libindy.dylib
+if [ ! -f /usr/local/lib/libindy.dylib ]; then
+    ln -s $LIBINDY_PATH /usr/local/lib/libindy.dylib
+fi
 otool -L /usr/local/lib/libindy.dylib
 
-ln -sf $VCXHEADER_PATH /usr/local/include/vcx.h
+if [ ! -f /usr/local/include/vcx.h ]; then
+    ln -s $VCXHEADER_PATH /usr/local/include/vcx.h
+fi
 
 for h in `ls $LIBINDY_HEADER_PATH`
 do
-    ln -sf $LIBINDY_HEADER_PATH/$h /usr/local/include/$h
+    if [ ! -f /usr/local/include/$h ]; then
+        ln -s $LIBINDY_HEADER_PATH/$h /usr/local/include/$h
+    fi
 done
--- vcx/libvcx/mac.06.libvcx.build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-source ./mac.05.libvcx.env.sh
-cargo clean
-cargo build
-cargo test
