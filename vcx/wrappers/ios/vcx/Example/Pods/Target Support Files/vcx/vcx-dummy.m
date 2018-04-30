9c6ca9931189a035f246e7b39401cf31791f58cb
yaswanthsvist <Evernym@Surabhis-MacBook-Pro.local>
2018-04-30 12:01:28 +0530

    added ios wrapper project

---
+++ vcx/wrappers/ios/vcx/Example/Pods/Target Support Files/vcx/vcx-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_vcx : NSObject
+@end
+@implementation PodsDummy_vcx
+@end
---
+++ vcx/wrappers/ios/vcx/Example/vcx/VCXViewController.m
@@ -0,0 +1,29 @@
+//
+//  VCXViewController.m
+//  vcx
+//
+//  Created by yaswanthsvist on 04/27/2018.
+//  Copyright (c) 2018 yaswanthsvist. All rights reserved.
+//
+
+#import "VCXViewController.h"
+
+@interface VCXViewController ()
+
+@end
+
+@implementation VCXViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
