1f249ffb078481a7f098790f4df27acc1ac2f716
yaswanthsvist <Evernym@Surabhis-MacBook-Pro.local>
2018-05-01 14:17:19 +0530

    added init interface with example code

---
+++ vcx/wrappers/ios/vcx/vcx/Classes/init.h
@@ -0,0 +1,16 @@
+//
+//  init.h
+//  vcx
+//
+//  Created by yaswanthsvist on 4/30/18.
+//
+
+#ifndef init_h
+#define init_h
+@interface Console : NSObject
+
++ ( void )log:(NSString *)message;
+
+@end
+
+#endif /* init_h */
