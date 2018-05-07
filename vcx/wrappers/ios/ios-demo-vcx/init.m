09656ab3e49cb03febb5d3dc8f781b46dcbef4db
Norman Jarvis <norman.jarvis@evernym.com>
2018-05-07 10:48:48 -0600

    [CO-1313] - An iOS phone project to show how to use libvcx to call into the libindy functions

---
+++ vcx/wrappers/ios/ios-demo-vcx/init.m
@@ -0,0 +1,19 @@
+//
+//  init.m
+//  vcx
+//
+//  Created by GuestUser on 4/30/18.
+//  Copyright © 2018 GuestUser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "init.h"
+//#include "vcx.h"
+@implementation Console
+
++ (void)log:(NSString *)message {
+    NSLog(@"###########%@",message);
+}
+
+
+@end
---
+++ vcx/wrappers/ios/ios-demo-vcx/vcx-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vcx-demo
+//
+//  Created by yaswanthsvist on 4/30/18.
+//  Copyright © 2018 GuestUser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
