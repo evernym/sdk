c2a618df8a7a5aa0d50e4a8c6a6b3e57cfc43a8e
Norman Jarvis <norman.jarvis@gmail.com>
2018-05-08 16:27:48 -0600

    [CO-1313] - Added a script to find the native dependencies that need to be built for iOS (#262)

--- vcx/libvcx/build_scripts/macos/mac.06.libvcx.build.sh
+++ vcx/libvcx/build_scripts/macos/mac.06.libvcx.build.sh
@@ -9,7 +9,7 @@ cargo clean
 #export RUST_BACKTRACE=1
 # To build for iOS
 #LIBINDY_DIR=/usr/local/lib RUST_BACKTRACE=1 cargo lipo --release
-cargo lipo --release
+cargo lipo --release --verbose
 #LIBINDY_DIR=/usr/local/lib RUST_BACKTRACE=1 cargo lipo
 #LIBINDY_DIR=/usr/local/lib cargo test
 cargo test
