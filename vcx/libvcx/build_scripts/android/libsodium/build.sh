e4bf2db2c0c0536c50f4808375ff959e8b681147
Michael Lodder <redmike7@gmail.com>
2018-05-10 16:30:51 -0600

    Added libsodium, fixed openssl
    Signed-off-by: Michael Lodder <redmike7@gmail.com>

---
+++ vcx/libvcx/build_scripts/android/libsodium/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+TARGET_ARCH=$1
+TARGET_API=$2
+CROSS_COMPILE=$3
+
+if [ -z "${TARGET_ARCH}" ]; then
+    echo STDERR "Missing TARGET_ARCH argument"
+    echo STDERR "e.g. x86 or arm"
+    exit 1 
+fi
+
+if [ -z "${TARGET_API}" ]; then
+    echo STDERR "Missing TARGET_API argument"
+    echo STDERR "e.g. 21"
+    exit 1 
+fi
+
+if [ -z "${CROSS_COMPILE}" ]; then
+    echo STDERR "Missing CROSS_COMPILE argument"
+    echo STDERR "e.g. i686-linux-android"
+    exit 1 
+fi
+
+if [ ! -f "android-ndk-r16b-linux-x86_64.zip" ] ; then
+    echo "Downloading android-ndk-r16b-linux-x86_64.zip"
+    wget -q https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip 
+else
+    echo "Skipping download android-ndk-r16b-linux-x86_64.zip"
+fi
+
+if [ ! -f "libsodium-1.0.12.tar.gz" ] ; then
+    echo "Downloading libsodium-1.0.12.tar.gz"
+    wget -q wget https://github.com/jedisct1/libsodium/releases/download/1.0.12/libsodium-1.0.12.tar.gz
+else
+    echo "Skipping download libsodium-1.0.12.tar.gz"
+fi
+
+docker build . --build-arg TARGET_ARCH=${TARGET_ARCH} --build-arg TARGET_API=${TARGET_API} --build-arg CROSS_COMPILE=${CROSS_COMPILE}
---
+++ vcx/libvcx/build_scripts/android/libsodium/make_libsodium.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export PATH=${TOOLCHAIN_DIR}/bin:${PATH}
+export CC=${TOOLCHAIN_DIR}/bin/${CROSS_COMPILE}-clang
+export AR=${TOOLCHAIN_DIR}/bin/${CROSS_COMPILE}-ar
+export CXX=${TOOLCHAIN_DIR}/bin/${CROSS_COMPILE}-clang++
+export CXXLD=${TOOLCHAIN_DIR}/bin/${CROSS_COMPILE}-ld
+export RANLIB=${TOOLCHAIN_DIR}/bin/${CROSS_COMPILE}-ranlib
+cd libsodium-1.0.12
+./autogen.sh
+./configure --prefix=${HOME}/libsodium_${TARGET_ARCH} --disable-soname-versions --host=${CROSS_COMPILE}
+make
+make install
+echo "libsodium android build successful"
