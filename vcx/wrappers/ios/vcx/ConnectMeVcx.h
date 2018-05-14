6f22846577374b2f4d7370323c82454eff7300b1
yaswanthsvist <Evernym@Surabhis-MacBook-Pro.local>
2018-05-14 15:56:35 +0530

    removed ios-demo-vcx project

---
+++ vcx/wrappers/ios/vcx/ConnectMeVcx.h
@@ -0,0 +1,97 @@
+//
+//  init.h
+//  vcx
+//
+//  Created by GuestUser on 4/30/18.
+//  Copyright © 2018 GuestUser. All rights reserved.
+//
+
+#ifndef init_h
+#define init_h
+#import "libvcx.h"
+
+extern void VcxWrapperCommonCallback(vcx_command_handle_t xcommand_handle,
+                                     vcx_error_t err);
+
+extern void VcxWrapperCommonHandleCallback(vcx_command_handle_t xcommand_handle,
+                                           vcx_error_t err,
+                                           vcx_command_handle_t pool_handle);
+
+extern void VcxWrapperCommonStringCallback(vcx_command_handle_t xcommand_handle,
+                                           vcx_error_t err,
+                                           const char *const arg1);
+
+extern void VcxWrapperCommonBoolCallback(vcx_command_handle_t xcommand_handle,
+                                         vcx_error_t err,
+                                         unsigned int arg1);
+
+extern void VcxWrapperCommonStringStringCallback(vcx_command_handle_t xcommand_handle,
+                                                 vcx_error_t err,
+                                                 const char *const arg1,
+                                                 const char *const arg2);
+
+extern void VcxWrapperCommonStringOptStringCallback(vcx_command_handle_t xcommand_handle,
+                                                    vcx_error_t err,
+                                                    const char *const arg1,
+                                                    const char *const arg2);
+
+extern void VcxWrapperCommonDataCallback(vcx_command_handle_t xcommand_handle,
+                                         vcx_error_t err,
+                                         const uint8_t *const arg1,
+                                         uint32_t arg2);
+
+extern void VcxWrapperCommonStringStringStringCallback(vcx_command_handle_t xcommand_handle,
+                                                       vcx_error_t err,
+                                                       const char *const arg1,
+                                                       const char *const arg2,
+                                                       const char *const arg3);
+
+extern void VcxWrapperCommonStringDataCallback(vcx_command_handle_t xcommand_handle,
+                                               vcx_error_t err,
+                                               const char *const arg1,
+                                               const uint8_t *const arg2,
+                                               uint32_t arg3);
+
+extern void VcxWrapperCommonNumberCallback(vcx_command_handle_t xcommand_handle,
+                                           vcx_error_t err,
+                                           int32_t handle);
+
+extern void VcxWrapperCommonStringOptStringOptStringCallback(vcx_command_handle_t xcommand_handle,
+                                                             vcx_error_t err,
+                                                             const char *const arg1,
+                                                             const char *const arg2,
+                                                             const char *const arg3);
+
+void VcxWrapperCommonStringStringLongCallback(vcx_command_handle_t xcommand_handle,
+                                              vcx_error_t err,
+                                              const char *arg1,
+                                              const char *arg2,
+                                              unsigned long long arg3);
+
+
+@interface ConnectMeVcx : NSObject
+
+/**
+ Calls when peer accepts a connection request from remoteDid.
+ 
+ Alghoritm:
+ 1. Prepare pool and wallet if needed.
+ 2. Check if a pairwiseDid is already stored for remoteDid
+ 3. If pairwise is not stored:
+     Create myDid with IndySignus:createAndStoreMyDid.
+     Create pairwise pair remoteDid:myDid and store in wallet with IndyPairwise: createPairwise.
+ 4. Store metadata.
+ 5. Return completion block, containing error and json in format: {"verificationKey": "generatedVerificationKey", "userDID": "generated pairwise DID for passed remoteDID"}
+ 
+ 
+ @param remoteDid Id of receiver identity.
+ @param remoteVerkey Verification key of remoteDID.
+ @param metadata Optional. Dictionaty with format: {String: Any}.
+ @param completion Completion block, returns error and json with info about generated pairwise did. Will be invoked in Main thread.
+ */
+- (void)agentProvision:(NSString *)config
+            completion:(void (^)(NSError *error, NSString *config))completion;
+
+@end
+
+#endif /* init_h */
---
+++ vcx/wrappers/ios/vcx/ConnectMeVcx.m
@@ -0,0 +1,280 @@
+//
+//  init.m
+//  vcx
+//
+//  Created by GuestUser on 4/30/18.
+//  Copyright © 2018 GuestUser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ConnectMeVcx.h"
+#import "utils/NSError+VcxError.h"
+#import "utils/VcxCallbacks.h"
+#import "vcx.h"
+#include "vcx.h"
+
+void VcxWrapperCommonCallback(vcx_command_handle_t xcommand_handle,
+                              vcx_error_t err) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *) = (void (^)(NSError *)) block;
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error);
+        });
+    }
+}
+
+void VcxWrapperCommonHandleCallback(vcx_command_handle_t xcommand_handle,
+                                    vcx_error_t err,
+                                    vcx_command_handle_t pool_handle) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, VcxHandle) = (void (^)(NSError *, VcxHandle)) block;
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, (VcxHandle) pool_handle);
+        });
+    }
+}
+
+void VcxWrapperCommonNumberCallback(vcx_command_handle_t xcommand_handle,
+                                    vcx_error_t err,
+                                    int32_t handle) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, NSNumber *) = (void (^)(NSError *, NSNumber *arg1)) block;
+    NSNumber *sarg1 = @(handle);
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, (NSNumber *) sarg1);
+        });
+    }
+}
+
+void VcxWrapperCommonStringCallback(vcx_command_handle_t xcommand_handle,
+                                    vcx_error_t err,
+                                    const char *const arg1) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, NSString *) = (void (^)(NSError *, NSString *arg1)) block;
+    NSString *sarg1 = [NSString stringWithUTF8String:arg1];
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, sarg1);
+        });
+    }
+}
+
+void VcxWrapperCommonBoolCallback(vcx_command_handle_t xcommand_handle,
+                                  vcx_error_t err,
+                                  unsigned int arg1) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, BOOL) = (void (^)(NSError *, BOOL arg1)) block;
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, (BOOL) arg1);
+        });
+    }
+}
+
+void VcxWrapperCommonStringStringCallback(vcx_command_handle_t xcommand_handle,
+                                          vcx_error_t err,
+                                          const char *const arg1,
+                                          const char *const arg2) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, NSString *arg1, NSString *arg2) = (void (^)(NSError *, NSString *arg1, NSString *arg2)) block;
+    
+    NSString *sarg1 = [NSString stringWithUTF8String:arg1];
+    NSString *sarg2 = [NSString stringWithUTF8String:arg2];
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, sarg1, sarg2);
+        });
+    }
+}
+
+void VcxWrapperCommonStringOptStringCallback(vcx_command_handle_t xcommand_handle,
+                                             vcx_error_t err,
+                                             const char *const arg1,
+                                             const char *const arg2) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, NSString *arg1, NSString *arg2) = (void (^)(NSError *, NSString *arg1, NSString *arg2)) block;
+    
+    NSString *sarg1 = [NSString stringWithUTF8String:arg1];
+    NSString *sarg2 = nil;
+    if (arg1) {
+        sarg2 = [NSString stringWithUTF8String:arg2];
+    }
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, sarg1, sarg2);
+        });
+    }
+}
+
+void VcxWrapperCommonStringOptStringOptStringCallback(vcx_command_handle_t xcommand_handle,
+                                                      vcx_error_t err,
+                                                      const char *const arg1,
+                                                      const char *const arg2,
+                                                      const char *const arg3) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, NSString *arg1, NSString *arg2, NSString *arg3) = (void (^)(NSError *, NSString *arg1, NSString *arg2, NSString *arg3)) block;
+    
+    NSString *sarg1 = [NSString stringWithUTF8String:arg1];
+    NSString *sarg2 = nil;
+    if (arg2) {
+        sarg2 = [NSString stringWithUTF8String:arg2];
+    }
+    NSString *sarg3 = nil;
+    if (arg3) {
+        sarg3 = [NSString stringWithUTF8String:arg3];
+    }
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, sarg1, sarg2, sarg3);
+        });
+    }
+}
+
+void VcxWrapperCommonStringStringStringCallback(vcx_command_handle_t xcommand_handle,
+                                                vcx_error_t err,
+                                                const char *const arg1,
+                                                const char *const arg2,
+                                                const char *const arg3) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, NSString *arg1, NSString *arg2, NSString *arg3) = (void (^)(NSError *, NSString *arg1, NSString *arg2, NSString *arg3)) block;
+    
+    NSString *sarg1 = [NSString stringWithUTF8String:arg1];
+    NSString *sarg2 = [NSString stringWithUTF8String:arg2];
+    NSString *sarg3 = [NSString stringWithUTF8String:arg3];
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, sarg1, sarg2, sarg3);
+        });
+    }
+}
+
+/// Arguments arg1 and arg2 will be converted to nsdata
+void VcxWrapperCommonDataCallback(vcx_command_handle_t xcommand_handle,
+                                  vcx_error_t err,
+                                  const uint8_t *const arg1,
+                                  uint32_t arg2) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, NSData *arg) = (void (^)(NSError *, NSData *arg)) block;
+    
+    NSData *sarg = [NSData dataWithBytes:arg1 length:arg2];
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, sarg);
+        });
+    }
+}
+
+void VcxWrapperCommonStringDataCallback(vcx_command_handle_t xcommand_handle,
+                                        vcx_error_t err,
+                                        const char *const arg1,
+                                        const uint8_t *const arg2,
+                                        uint32_t arg3) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, NSString *, NSData *) = (void (^)(NSError *, NSString *, NSData *)) block;
+    
+    NSString *sarg1 = nil;
+    if (arg1) {
+        sarg1 = [NSString stringWithUTF8String:arg1];
+    }
+    NSData *sarg2 = [NSData dataWithBytes:arg2 length:arg3];
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, sarg1, sarg2);
+        });
+    }
+}
+
+void VcxWrapperCommonStringStringLongCallback(vcx_command_handle_t xcommand_handle,
+                                              vcx_error_t err,
+                                              const char *arg1,
+                                              const char *arg2,
+                                              unsigned long long arg3) {
+    id block = [[VcxCallbacks sharedInstance] commandCompletionFor:xcommand_handle];
+    [[VcxCallbacks sharedInstance] deleteCommandHandleFor:xcommand_handle];
+    
+    void (^completion)(NSError *, NSString *, NSString *, NSNumber *) = (void (^)(NSError *, NSString *arg1, NSString *arg2, NSNumber *arg3)) block;
+    NSString *sarg1 = [NSString stringWithUTF8String:arg1];
+    NSString *sarg2 = [NSString stringWithUTF8String:arg2];
+    NSNumber *sarg3 = [NSNumber numberWithInt:arg3];
+    
+    
+    if (completion) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSError *error = [NSError errorFromVcxError:err];
+            completion(error, (NSString *) sarg1, (NSString *) sarg2, (NSNumber *) sarg3);
+        });
+    }
+}
+
+
+
+
+@implementation ConnectMeVcx
+
+- (void)agentProvision:(NSString *)config completion:(void (^)(NSError *error, NSString *config))completion{
+    const char *config_char = [config cString];
+    vcx_command_handle_t handle= [[VcxCallbacks sharedInstance] createCommandHandleFor:completion] ;
+    vcx_error_t ret = vcx_agent_provision_async(handle, config_char, VcxWrapperCommonStringCallback);
+    if( ret != 0 )
+    {
+        [[VcxCallbacks sharedInstance] deleteCommandHandleFor: handle];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"agentProvision: calling completion");
+            completion([NSError errorFromVcxError: ret], false);
+        });
+    }
+    
+}
+
+
+
+
+@end
