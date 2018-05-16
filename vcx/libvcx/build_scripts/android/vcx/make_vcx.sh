0fe0867eed11107a7c3ae12d16b82adc737ab1d4
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-05-16 10:59:00 +0530

    Add logging support to libvcx

--- vcx/libvcx/build_scripts/android/vcx/make_vcx.sh
+++ vcx/libvcx/build_scripts/android/vcx/make_vcx.sh
@@ -16,5 +16,5 @@ rustup target add ${CROSS_COMPILE}
 cd "${HOME}/sdk/vcx/libvcx"
 export OPENSSL_STATIC=1
 cargo build --release --target=${CROSS_COMPILE}
-$CC -shared -o ${HOME}/libvcx.so -Wl,--whole-archive ${HOME}/sdk/vcx/libvcx/target/${CROSS_COMPILE}/release/libvcx.a ${TOOLCHAIN_DIR}/sysroot/usr/lib/libz.a ${TOOLCHAIN_DIR}/sysroot/usr/lib/libm.a ${LIBINDY_DIR}/libindy.a ${OPENSSL_DIR}/lib/libssl.a ${OPENSSL_DIR}/lib/libcrypto.a ${SODIUM_LIB_DIR}/libsodium.a ${LIBZMQ_LIB_DIR}/libzmq.a ${TOOLCHAIN_DIR}/${CROSS_COMPILE}/lib/libstdc++.a -Wl,--no-whole-archive -z muldefs
+$CC -shared -o ${HOME}/libvcx.so -Wl,--whole-archive ${HOME}/sdk/vcx/libvcx/target/${CROSS_COMPILE}/release/libvcx.a ${TOOLCHAIN_DIR}/sysroot/usr/lib/libz.a ${TOOLCHAIN_DIR}/sysroot/usr/lib/libm.a ${TOOLCHAIN_DIR}/sysroot/usr/lib/liblog.so ${LIBINDY_DIR}/libindy.a ${OPENSSL_DIR}/lib/libssl.a ${OPENSSL_DIR}/lib/libcrypto.a ${SODIUM_LIB_DIR}/libsodium.a ${LIBZMQ_LIB_DIR}/libzmq.a ${TOOLCHAIN_DIR}/${CROSS_COMPILE}/lib/libstdc++.a -Wl,--no-whole-archive -z muldefs
 cp "${HOME}/sdk/vcx/libvcx/target/${CROSS_COMPILE}/release/libvcx.a" ${HOME}/
