1baf12ae6f309fb4cda23831e40c09bd318da984
Norman Jarvis <norman.jarvis@evernym.com>
2018-05-18 09:54:25 -0600

    Fixed the missing architecture i386 in the libzmq.a and libsodium.a static libraries

--- vcx/libvcx/build_scripts/ios/mac/mac.09.libzmq.libsodium.build.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.09.libzmq.libsodium.build.sh
@@ -10,11 +10,10 @@ WORK_DIR=$(abspath "$WORK_DIR")
 if [ -d $WORK_DIR/libzmq-ios ]; then
     rm -rf $WORK_DIR/libzmq-ios
 fi
-git clone https://github.com/drewcrawford/libzmq-ios.git $WORK_DIR/libzmq-ios
+git clone https://github.com/azawawi/libzmq-ios.git $WORK_DIR/libzmq-ios
 cd $WORK_DIR/libzmq-ios
-git clone https://github.com/drewcrawford/libsodium-ios.git $WORK_DIR/libzmq-ios/libsodium-ios
-sed -i.bak 's/4\.1\.5/4.1.6/g' ./libzmq.sh
-sed -i.bak 's/i386\ //' ./libzmq.sh
-sed -i.bak 's/0\.6\.1/1.0.12/g' ./libsodium-ios/libsodium.sh
-sed -i.bak 's/i386\ //' ./libsodium-ios/libsodium.sh
-./libzmq.sh
+git clone https://github.com/azawawi/libsodium-ios.git $WORK_DIR/libzmq-ios/libsodium-ios
+cd $WORK_DIR/libzmq-ios/libsodium-ios
+./libsodium.rb
+cd ..
+./libzmq.rb
