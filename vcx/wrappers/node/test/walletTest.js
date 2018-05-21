242713c456df4c03d974b4aca0220a1c857f042c
Mark Hadley <hadleym@email.arizona.edu>
2018-05-21 08:31:03 -0600

    Wallet Storage API for Rust, Python, and Node (#270)
    Adds add_record Call
    Adds vcx_wallet_update_record_value Call
    Adds vcx_wallet_update_record_tags Call
    Adds vcx_wallet_add_record_tags Call
    Adds vcx_wallet_delete_record_tags Call
    Adds vcx_wallet_delete_record Call
    Adds vcx_wallet_get_record Call
    Adds vcx_wallet_open_search Call
    Adds vcx_wallet_close_search Call
    Adds vcx_wallet_search_next_records Call

--- vcx/wrappers/node/test/walletTest.js
+++ vcx/wrappers/node/test/walletTest.js
@@ -4,6 +4,35 @@ const { stubInitVCX } = require('./helpers')
 const assert = chai.assert
 
 const { Wallet } = vcx
+const WALLET_RECORD = {
+  type_: 'TestType',
+  id: 'RecordId',
+  value: 'RecordValue',
+  tags: {}
+}
+
+const QUERY_JSON = {'tagName1': 'str1'}
+
+const UPDATE_WALLET_RECORD = {
+  type_: 'TestType',
+  id: 'RecordId',
+  value: 'RecordValueNew'
+}
+const UPDATE_WALLET_TAGS = {
+  type_: 'TestType',
+  id: 'RecordId',
+  value: '',
+  tags: {}
+}
+
+const TAGS = '{"tagName1":"str1","tagName2":"5","tagName3":"12"}'
+
+const SEARCHED_RECORD = {
+  'id': 'RecordId',
+  'type': null,
+  'value': 'RecordValue',
+  'tags': TAGS
+}
 
 describe('A Connection object with ', function () {
   this.timeout(10000)
@@ -23,5 +52,22 @@ describe('A Connection object with ', function () {
   it('can send tokens', async () => {
     const receipt = await Wallet.sendTokens(0, 30, 'address')
     assert(receipt)
+  })
+
+  // wallet store
+  it.only('can perform record operations', async () => {
+    await Wallet.addRecord(WALLET_RECORD)
+    await Wallet.getRecord(WALLET_RECORD.type_, WALLET_RECORD.id)
+    await Wallet.updateRecordValue(UPDATE_WALLET_RECORD)
+    await Wallet.updateRecordTags(UPDATE_WALLET_TAGS)
+    await Wallet.addRecordTags(UPDATE_WALLET_TAGS)
+    await Wallet.deleteRecordTags(WALLET_RECORD, ['one', 'two'])
+    await Wallet.deleteRecord(WALLET_RECORD.type_, WALLET_RECORD.id)
+    var searchHandle = await Wallet.openSearch(WALLET_RECORD.type_, JSON.stringify(QUERY_JSON), 'null')
+    console.log('searchHandle: ' + searchHandle)
+    assert(searchHandle === 1)
+    var retrievedRecords = JSON.parse(await Wallet.searchNextRecords(searchHandle, 1))
+    assert(JSON.stringify(retrievedRecords) === JSON.stringify(SEARCHED_RECORD))
+    await Wallet.closeSearch(searchHandle)
   })
 })
