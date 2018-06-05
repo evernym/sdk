2ccf8f0b5a8e6cfd4ee41504bf97a33e4c6c730e
Norman Jarvis <norman.jarvis@evernym.com>
2018-06-05 12:07:29 -0600

    Added the sudo command infront of the cp command so that it can be copied to /usr/local

--- vcx/libvcx/build_scripts/android/mac/mac.upload.android.build.zipfiles.sh
+++ vcx/libvcx/build_scripts/android/mac/mac.upload.android.build.zipfiles.sh
@@ -19,20 +19,20 @@ cd $VCX_SDK/vcx/wrappers/java/android/vcxtest/app/jni
 
 mv libvcxall_arm.zip libvcxall_${DATETIME}_arm.zip
 curl --insecure -u normjarvis -X POST -F file=@./libvcxall_${DATETIME}_arm.zip https://kraken.corp.evernym.com/repo/android/upload
-cp ./libvcxall_${DATETIME}_arm.zip  /usr/local/var/www/download/android
+sudo cp -v ./libvcxall_${DATETIME}_arm.zip  /usr/local/var/www/download/android
 
 mv libvcxall_arm64.zip libvcxall_${DATETIME}_arm64.zip
 curl --insecure -u normjarvis -X POST -F file=@./libvcxall_${DATETIME}_arm64.zip https://kraken.corp.evernym.com/repo/android/upload
-cp ./libvcxall_${DATETIME}_arm64.zip  /usr/local/var/www/download/android
+sudo cp -v ./libvcxall_${DATETIME}_arm64.zip  /usr/local/var/www/download/android
 
 mv libvcxall_armv7.zip libvcxall_${DATETIME}_armv7.zip
 curl --insecure -u normjarvis -X POST -F file=@./libvcxall_${DATETIME}_armv7.zip https://kraken.corp.evernym.com/repo/android/upload
-cp ./libvcxall_${DATETIME}_armv7.zip  /usr/local/var/www/download/android
+sudo cp -v ./libvcxall_${DATETIME}_armv7.zip  /usr/local/var/www/download/android
 
 mv libvcxall_x86.zip libvcxall_${DATETIME}_x86.zip
 curl --insecure -u normjarvis -X POST -F file=@./libvcxall_${DATETIME}_x86.zip https://kraken.corp.evernym.com/repo/android/upload
-cp ./libvcxall_${DATETIME}_x86.zip  /usr/local/var/www/download/android
+sudo cp -v ./libvcxall_${DATETIME}_x86.zip  /usr/local/var/www/download/android
 
 mv libvcxall_x86_64.zip libvcxall_${DATETIME}_x86-64.zip
 curl --insecure -u normjarvis -X POST -F file=@./libvcxall_${DATETIME}_x86-64.zip https://kraken.corp.evernym.com/repo/android/upload
-cp ./libvcxall_${DATETIME}_x86-64.zip  /usr/local/var/www/download/android
+sudo cp -v ./libvcxall_${DATETIME}_x86-64.zip  /usr/local/var/www/download/android
