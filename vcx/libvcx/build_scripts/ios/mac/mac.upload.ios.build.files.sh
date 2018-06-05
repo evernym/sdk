d45306375907a1ee934aecc416c06d25f552ce62
Norman Jarvis <norman.jarvis@evernym.com>
2018-06-05 10:31:52 -0600

    Added the steps to automate the creation of the cocoapod archive file that the ConnectMe build relies on

--- vcx/libvcx/build_scripts/ios/mac/mac.upload.ios.build.files.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.upload.ios.build.files.sh
@@ -18,8 +18,37 @@ fi
 
 cd $VCX_SDK/vcx/wrappers/ios/vcx/lib
 mv libvcx.a libvcx.a.original
-cp libvcxall.a libvcx.a
-tar zcf libvcx.a_${DATETIME}_universal.tar.gz libvcx.a
+cp -v libvcxall.a libvcx.a
+rm libvcx.a_${DATETIME}_universal.tar.gz
+tar vzcf libvcx.a_${DATETIME}_universal.tar.gz libvcx.a
 mv libvcx.a.original libvcx.a
 curl --insecure -u normjarvis -X POST -F file=@$VCX_SDK/vcx/wrappers/ios/vcx/lib/libvcx.a_${DATETIME}_universal.tar.gz https://kraken.corp.evernym.com/repo/ios/upload
-#Download the file at https://repo.corp.evernym.com/filely/ios/libvcx.a_${DATETIME}_universal.tar.gz
+# Download the file at https://repo.corp.evernym.com/filely/ios/libvcx.a_${DATETIME}_universal.tar.gz
+
+cd $VCX_SDK/vcx/wrappers/ios/vcx
+mkdir -p vcx.framework/lib
+cp -v lib/libvcxall.a vcx.framework/lib/libvcx.a
+cp -v ConnectMeVcx.h vcx.framework/Headers
+cp -v include/libvcx.h vcx.framework/Headers
+cp -v vcx/vcx.h vcx.framework/Headers
+rm -rf $VCX_SDK/vcx/wrappers/ios/vcx/tmp
+mkdir -p $VCX_SDK/vcx/wrappers/ios/vcx/tmp/vcx/
+cp -rvp vcx.framework $VCX_SDK/vcx/wrappers/ios/vcx/tmp/vcx/
+cd $VCX_SDK/vcx/wrappers/ios/vcx/tmp
+rm vcx.framework_${DATETIME}_universal.zip
+zip -r vcx.framework_${DATETIME}_universal.zip vcx
+# |---vcx.framework_20180522.1635_universal.zip
+# |---vcx
+#      |---vcx.framework
+#            |----lib
+#            |       |---libvcx.a
+#            |----headers
+#            |       |---vcx.h
+#            |       |---ConnectMeVcx.h
+#            |       |---libvcx.h
+#            |----vcx
+#            |----Modules
+#            |       |---module.modulemap
+#            |----Info.plist
+curl --insecure -u normjarvis -X POST -F file=@./vcx.framework_${DATETIME}_universal.zip https://kraken.corp.evernym.com/repo/ios/upload
+# Download the file at https://repo.corp.evernym.com/filely/ios/vcx.framework_${DATETIME}_universal.zip
