b7e2853c25c981162bebafd6ffff74ab7a181317
Mark Hadley <mark.hadley@evernym.com>
2018-06-18 13:42:26 -0600

    Adds Testing to Python

--- vcx/wrappers/python3/ci/test-python.sh
+++ vcx/wrappers/python3/ci/test-python.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 DEBIAN=`ls /sdk/vcx/libvcx/target/debian/*.deb -rt | tail -1`
+PYTHON_DIR="/sdk/vcx/wrappers/python3"
 dpkg -i $DEBIAN
 ls /usr/lib/libvcx.so -al
+cd $PYTHON_DIR
+python3 test
