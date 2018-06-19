c288cd7f2e05df06d4cabd02b3c77fbc79b29eae
Ryan Marsh <ryan.marsh44@gmail.com>
2018-06-19 13:18:59 -0600

    package all .so

--- vcx/ci/scripts/androidPackage.sh
+++ vcx/ci/scripts/androidPackage.sh
@@ -5,8 +5,8 @@ ANDROID_JNI_LIB=vcx/wrappers/java/vcx/src/main/jniLibs
 echo $(ls runtime_android_build)
 echo $(ls runtime_android_build/libvcx_arm)
 cp -v runtime_android_build/libvcx_arm/libvcx.so ${ANDROID_JNI_LIB}/arm/libvcx.so
-#cp -v runtime_android_build/libvcx_x86/libvcx.so ${ANDROID_JNI_LIB}/x86
-#cp -v runtime_android_build/libvcx_arm64/libvcx.so ${ANDROID_JNI_LIB}/arm64
+cp -v runtime_android_build/libvcx_x86/libvcx.so ${ANDROID_JNI_LIB}/x86
+cp -v runtime_android_build/libvcx_arm64/libvcx.so ${ANDROID_JNI_LIB}/arm64
 
 echo $(ls vcx/wrappers/java/vcx/src/main/jniLibs)
 pushd vcx/wrappers/java/vcx
