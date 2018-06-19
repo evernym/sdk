59fe13af6faf01709473c2c8a43855a0dff3a4e7
Mark Hadley <mark.hadley@evernym.com>
2018-06-19 08:43:13 -0600

    Adds Second Script for Installing Debian To Container

--- vcx/ci/scripts/create-debian.sh
+++ vcx/ci/scripts/create-debian.sh
@@ -4,10 +4,7 @@ PYTHON_DIR="/sdk/vcx/wrappers/python3"
 cd $DIR
 python3 $SCRIPTS/cargo-update-version
 cargo build 
-cargo test -- --test-threads=1
+#cargo test -- --test-threads=1
 python3 $SCRIPTS/cargo-update-so
 cargo deb --no-build
-dpkg -i $DIR/target/debian/*.deb
-cd $PYTHON_DIR
-python3 -m pytest
 
