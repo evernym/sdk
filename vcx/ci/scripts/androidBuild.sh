e25a3dccb968c3680d8756c1751892280028cad1
Ryan Marsh <ryan.marsh44@gmail.com>
2018-06-19 10:15:35 -0600

    changed indy-sdk repo and added write permissions

--- vcx/ci/scripts/androidBuild.sh
+++ vcx/ci/scripts/androidBuild.sh
@@ -86,7 +86,8 @@ generate_flags(){
 clone_indy_sdk() {
     if [ ! -d "indy-sdk" ]; then
         echo "cloning indy-sdk"
-        git clone -b android_builds --single-branch https://github.com/faisal00813/indy-sdk.git
+        git clone https://github.com/evernym/indy-sdk.git
+        #git clone -b android_builds --single-branch https://github.com/faisal00813/indy-sdk.git
     fi
 }
 
