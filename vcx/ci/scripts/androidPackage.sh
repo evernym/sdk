0aa8dd926cefdd418a161b213c615eda5860c6e9
Ryan Marsh <ryan.marsh44@gmail.com>
2018-06-19 13:38:31 -0600

    update getCert script path

--- vcx/ci/scripts/androidPackage.sh
+++ vcx/ci/scripts/androidPackage.sh
@@ -3,16 +3,10 @@
 echo ${PWD}
 ANDROID_JNI_LIB=vcx/wrappers/java/vcx/src/main/jniLibs
 echo $(ls runtime_android_build)
-echo $(ls runtime_android_build/libvcx_arm)
 cp -v runtime_android_build/libvcx_arm/libvcx.so ${ANDROID_JNI_LIB}/arm/libvcx.so
 cp -v runtime_android_build/libvcx_x86/libvcx.so ${ANDROID_JNI_LIB}/x86
 cp -v runtime_android_build/libvcx_arm64/libvcx.so ${ANDROID_JNI_LIB}/arm64
 
-echo $(ls vcx/wrappers/java/vcx/src/main/jniLibs)
 pushd vcx/wrappers/java/vcx
     ./gradlew clean assemble
-    echo $(ls)
-    echo $(ls build/)
-    echo $(ls build/outputs/)
-    echo $(ls build/outputs/aar)
 popd
