348b3e732d22132ad500772552203dd3d380d721
Norman Jarvis <norman.jarvis@evernym.com>
2018-06-19 18:10:52 -0600

    Updated the comment to show that dylib was changed to cdylib

--- vcx/libvcx/build_scripts/android/mac/mac.03.libindy.build.sh
+++ vcx/libvcx/build_scripts/android/mac/mac.03.libindy.build.sh
@@ -128,7 +128,7 @@ cargo build --target x86_64-apple-darwin --release --verbose
 #########################################################################################################################
 cd $WORK_DIR/vcx-indy-sdk/libnullpay
 
-# Replace '\"dylib\"' with '\"staticlib\", \"dylib\"' in Cargo.toml
+# Replace '\"cdylib\"' with '\"staticlib\", \"cdylib\"' in Cargo.toml
 sed -i .bak 's/\"cdylib\"/\"staticlib\", \"cdylib\"/' Cargo.toml
 
 # !IMPORTANT STEPS NEXT -- Modify the build.rs of libnullpay to handle android shared libraries
--- vcx/libvcx/build_scripts/ios/mac/mac.03.libindy.build.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.03.libindy.build.sh
@@ -44,7 +44,7 @@ cargo lipo --release --verbose --targets="aarch64-apple-ios,armv7-apple-ios,armv
 #########################################################################################################################
 cd $WORK_DIR/vcx-indy-sdk/libnullpay
 
-# Replace '\"dylib\"' with '\"staticlib\", \"dylib\"' in Cargo.toml
+# Replace '\"cdylib\"' with '\"staticlib\", \"cdylib\"' in Cargo.toml
 sed -i .bak 's/\"cdylib\"/\"staticlib\", \"cdylib\"/' Cargo.toml
 
 # To build for macos
