23cd8f194a52ac034bddc19acd64c4ed3bc229dc
Norman Jarvis <norman.jarvis@evernym.com>
2018-06-20 12:37:06 -0600

    Added the steps to build libsqlite3 so that the android libindy build will work

--- vcx/libvcx/build_scripts/android/mac/mac.03.libindy.build.sh
+++ vcx/libvcx/build_scripts/android/mac/mac.03.libindy.build.sh
@@ -93,7 +93,7 @@ export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH
 export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi
 export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm/lib
 export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm/lib
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/armeabi-v7a
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/armeabi-v7a
 sed -i .bak 's/\"\"\.as_ptr() as \*const i8/\"\"\.as_ptr() as \*const u8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target arm-linux-androideabi --release --verbose
 
@@ -101,7 +101,7 @@ export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH
 export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi-v7a
 export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_armv7/lib
 export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_armv7/lib
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/armeabi-v7a
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/armeabi-v7a
 sed -i .bak 's/\"\"\.as_ptr() as \*const i8/\"\"\.as_ptr() as \*const u8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target armv7-linux-androideabi --release --verbose
 
@@ -109,7 +109,7 @@ export PATH=$WORK_DIR/NDK/arm64/bin:$ORIGINAL_PATH
 export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-arm64-v8a
 export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm64/lib
 export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm64/lib
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/arm64-v8a
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/arm64-v8a
 sed -i .bak 's/\"\"\.as_ptr() as \*const i8/\"\"\.as_ptr() as \*const u8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target aarch64-linux-android --release --verbose
 
@@ -117,7 +117,7 @@ export PATH=$WORK_DIR/NDK/x86/bin:$ORIGINAL_PATH
 export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86
 export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86/lib
 export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86/lib
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/x86
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/x86
 sed -i .bak 's/\"\"\.as_ptr() as \*const u8/\"\"\.as_ptr() as \*const i8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target i686-linux-android --release --verbose
 
@@ -125,7 +125,7 @@ export PATH=$WORK_DIR/NDK/x86_64/bin:$ORIGINAL_PATH
 export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86_64
 export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86_64/lib
 export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86_64/lib
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/x86_64
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/x86_64
 sed -i .bak 's/\"\"\.as_ptr() as \*const u8/\"\"\.as_ptr() as \*const i8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target x86_64-linux-android --release --verbose
 
