9e6a3f9b57da3cf710ab10d3bbf4cb0a92cfe65b
Norman Jarvis <norman.jarvis@evernym.com>
2018-06-21 10:17:45 -0600

    Changes to build the evernym fork of the indy-sdk repo

--- vcx/libvcx/build_scripts/android/mac/mac.03.libindy.build.sh
+++ vcx/libvcx/build_scripts/android/mac/mac.03.libindy.build.sh
@@ -27,6 +27,8 @@ git checkout `cat $SHA_HASH_DIR/libindy.commit.sha1.hash.txt`
 #git checkout tags/v1.3.0
 cd $WORK_DIR/vcx-indy-sdk/libindy
 
+export LIBZMQ_LIB_DIR=/usr/local/lib
+sed -i .bak 's/LIBZMQ_LIB_DIR/ANDROID_ZMQ_LIB/' build.rs
 # !IMPORTANT STEPS NEXT -- Modify the build.rs of indy-sdk to handle android shared libraries
 tail -n 1 build.rs | wc -c | xargs -I {} truncate build.rs -s -{}
 cat $START_DIR/indy-sdk.build.rs.android.target.static.libs.template >> build.rs
@@ -89,65 +91,73 @@ export OPENSSL_DIR_DARWIN=$OPENSSL_DIR
 #########################################################################################################################
 # Now build libindy
 #########################################################################################################################
-export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_arm/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/armeabi-v7a
+export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_arm/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/armeabi-v7a; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
 sed -i .bak 's/\"\"\.as_ptr() as \*const i8/\"\"\.as_ptr() as \*const u8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target arm-linux-androideabi --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
-export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi-v7a
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_armv7/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_armv7/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_armv7/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/armeabi-v7a
+export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi-v7a; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_armv7/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_armv7/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_armv7/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/armeabi-v7a; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
 sed -i .bak 's/\"\"\.as_ptr() as \*const i8/\"\"\.as_ptr() as \*const u8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target armv7-linux-androideabi --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
-export PATH=$WORK_DIR/NDK/arm64/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-arm64-v8a
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm64/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm64/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_arm64/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/arm64-v8a
+export PATH=$WORK_DIR/NDK/arm64/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-arm64-v8a; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm64/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm64/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_arm64/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/arm64-v8a; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
 sed -i .bak 's/\"\"\.as_ptr() as \*const i8/\"\"\.as_ptr() as \*const u8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target aarch64-linux-android --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
-export PATH=$WORK_DIR/NDK/x86/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_x86/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/x86
+export PATH=$WORK_DIR/NDK/x86/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_x86/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/x86; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
 sed -i .bak 's/\"\"\.as_ptr() as \*const u8/\"\"\.as_ptr() as \*const i8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target i686-linux-android --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
-export PATH=$WORK_DIR/NDK/x86_64/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86_64
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86_64/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86_64/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_x86_64/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/x86_64
+export PATH=$WORK_DIR/NDK/x86_64/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86_64; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86_64/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86_64/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_x86_64/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/sqlite3-android/obj/local/x86_64; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
 sed -i .bak 's/\"\"\.as_ptr() as \*const u8/\"\"\.as_ptr() as \*const i8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target x86_64-linux-android --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
 # This builds the library for code that runs in OSX
 export OPENSSL_DIR=$OPENSSL_DIR_DARWIN
+#unset LIBZMQ_LIB_DIR
+unset LIBZMQ_INCLUDE_DIR
 sed -i .bak 's/\"\"\.as_ptr() as \*const u8/\"\"\.as_ptr() as \*const i8/' src/services/wallet/storage/plugged/mod.rs
 cargo build --target x86_64-apple-darwin --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
 #########################################################################################################################
 # Now build libnullpay
@@ -162,64 +172,71 @@ tail -n 1 build.rs | wc -c | xargs -I {} truncate build.rs -s -{}
 cat $START_DIR/libnullpay.build.rs.android.target.static.libs.template >> build.rs
 ###################################################################################################
 
-export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_arm/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/armeabi-v7a
-export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/arm-linux-androideabi/release
-cargo build --target arm-linux-androideabi --release --verbose
+export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_arm/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/armeabi-v7a; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
+export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/arm-linux-androideabi/release; echo "LIBINDY_DIR: $LIBINDY_DIR"
+#cargo build --target arm-linux-androideabi --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
-export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi-v7a
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_armv7/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_armv7/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_armv7/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/armeabi-v7a
-export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/armv7-linux-androideabi/release
-cargo build --target armv7-linux-androideabi --release --verbose
+export PATH=$WORK_DIR/NDK/arm/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-armeabi-v7a; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_armv7/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_armv7/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_armv7/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/armeabi-v7a; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
+export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/armv7-linux-androideabi/release; echo "LIBINDY_DIR: $LIBINDY_DIR"
+#cargo build --target armv7-linux-androideabi --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
-export PATH=$WORK_DIR/NDK/arm64/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-arm64-v8a
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm64/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm64/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_arm64/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/arm64-v8a
-export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/aarch64-linux-android/release
-cargo build --target aarch64-linux-android --release --verbose
+export PATH=$WORK_DIR/NDK/arm64/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-arm64-v8a; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_arm64/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_arm64/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_arm64/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/arm64-v8a; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
+export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/aarch64-linux-android/release; echo "LIBINDY_DIR: $LIBINDY_DIR"
+#cargo build --target aarch64-linux-android --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
-export PATH=$WORK_DIR/NDK/x86/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_x86/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/x86
-export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/i686-linux-android/release
-cargo build --target i686-linux-android --release --verbose
+export PATH=$WORK_DIR/NDK/x86/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_x86/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/x86; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
+export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/i686-linux-android/release; echo "LIBINDY_DIR: $LIBINDY_DIR"
+#cargo build --target i686-linux-android --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
-export PATH=$WORK_DIR/NDK/x86_64/bin:$ORIGINAL_PATH
-export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86_64
-export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86_64/lib
-export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB
-export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86_64/lib
-export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB
-export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_x86_64/include
-export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/x86_64
-export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/x86_64-linux-android/release
-cargo build --target x86_64-linux-android --release --verbose
+export PATH=$WORK_DIR/NDK/x86_64/bin:$ORIGINAL_PATH; echo "PATH: $PATH"
+export OPENSSL_DIR=$WORK_DIR/openssl_for_ios_and_android/output/android/openssl-x86_64; echo "OPENSSL_DIR: $OPENSSL_DIR"
+export ANDROID_SODIUM_LIB=$WORK_DIR/libzmq-android/libsodium/libsodium_x86_64/lib; echo "ANDROID_SODIUM_LIB: $ANDROID_SODIUM_LIB"
+export SODIUM_LIB_DIR=$ANDROID_SODIUM_LIB; echo "SODIUM_LIB_DIR: $SODIUM_LIB_DIR"
+export ANDROID_ZMQ_LIB=$WORK_DIR/libzmq-android/zmq/libzmq_x86_64/lib; echo "ANDROID_ZMQ_LIB: $ANDROID_ZMQ_LIB"
+#export LIBZMQ_LIB_DIR=$ANDROID_ZMQ_LIB; echo "LIBZMQ_LIB_DIR: $LIBZMQ_LIB_DIR"
+export LIBZMQ_INCLUDE_DIR=$WORK_DIR/libzmq-android/zmq/libzmq_x86_64/include; echo "LIBZMQ_INCLUDE_DIR: $LIBZMQ_INCLUDE_DIR"
+export ANDROID_SQLITE_LIB=$WORK_DIR/libsqlite3-android/x86_64; echo "ANDROID_SQLITE_LIB: $ANDROID_SQLITE_LIB"
+export LIBINDY_DIR=$WORK_DIR/vcx-indy-sdk/libindy/target/x86_64-linux-android/release; echo "LIBINDY_DIR: $LIBINDY_DIR"
+#cargo build --target x86_64-linux-android --release --verbose
+echo "-----------------------------------------------------------------------------------------------"
 
 # This builds the library for code that runs in OSX
 export OPENSSL_DIR=$OPENSSL_DIR_DARWIN
-cargo build --target x86_64-apple-darwin --release --verbose
+#unset LIBZMQ_LIB_DIR
+unset LIBZMQ_INCLUDE_DIR
+#cargo build --target x86_64-apple-darwin --release --verbose
 
 export PATH=$ORIGINAL_PATH
 #export PKG_CONFIG_PATH=$ORIGINAL_PKG_CONFIG_PATH
