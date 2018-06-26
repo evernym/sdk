370c3e523b37334d35baebaa7abacbbaf587ceb7
Ryan Marsh <ryan.marsh44@gmail.com>
2018-06-26 12:53:11 -0600

    added print6

--- vcx/ci/scripts/iosBuild.sh
+++ vcx/ci/scripts/iosBuild.sh
@@ -5,38 +5,34 @@ export BASE_DIR="../../../../.."
 
 
 ls
+# take this out
 mkdir .macosbuild
 cd ${SCRIPTS_PATH}
-ls -al
-ls ~
 #./mac.02.libindy.env.sh
 #./mac.03.libindy.build.sh
 #./mac.04.libvcx.setup.sh
 #source ./mac.05.libvcx.env.sh
 #./mac.06.libvcx.build.sh
 cp -rf ~/OpenSSL-for-iPhone ${BASE_DIR}/.macosbuild
-ls ~/OpenSSL-for-iPhone
-ls ~/OpenSSL-for-iPhone/lib
+cp -rf ~/libzmq-ios ${BASE_DIR}/.macosbuild
+cp -rf ~/combine-libs ${BASE_DIR}/.macosbuild
+echo '****************************'
+ls ~
+echo '-----------------------------'
 ls ${BASE_DIR}/.macosbuild
-# cp -rf ~/libzmq-ios ${BASE_DIR}/.macosbuild
-# cp -rf ~/combine-libs ${BASE_DIR}/.macosbuild
-# echo '****************************'
-# ls ~
-# echo '-----------------------------'
-# ls ${BASE_DIR}/.macosbuild
-# echo '-----------------------------'
-# ls ${BASE_DIR}/.macosbuild/OpenSSL-for-iPhone/lib/
-# echo '-----------------------------'
-# ls ${BASE_DIR}/.macosbuild/libzmq-ios/dist/ios/lib
-# echo '-----------------------------'
-# ls ${BASE_DIR}/.macosbuild/libzmq-ios/libsodium-ios/dist/ios/lib
-# echo '-----------------------------'
-# ls ${BASE_DIR}/.macosbuild/combine-libs/libsqlite3
-# echo '-----------------------------'
-# ls ${BASE_DIR}/.macosbuild/combine-libs/libminiz
-# cd ${BASE_DIR}
-# pwd
-# cd ${SCRIPTS_PATH}
-# echo '****************************'
-# ./mac.11.copy.static.libs.to.app.sh
-# ./mac.12.combine.static.libs.sh libvcxall
+echo '-----------------------------'
+ls ${BASE_DIR}/.macosbuild/OpenSSL-for-iPhone/lib/
+echo '-----------------------------'
+ls ${BASE_DIR}/.macosbuild/libzmq-ios/dist/ios/lib
+echo '-----------------------------'
+ls ${BASE_DIR}/.macosbuild/libzmq-ios/libsodium-ios/dist/ios/lib
+echo '-----------------------------'
+ls ${BASE_DIR}/.macosbuild/combine-libs/libsqlite3
+echo '-----------------------------'
+ls ${BASE_DIR}/.macosbuild/combine-libs/libminiz
+cd ${BASE_DIR}
+pwd
+cd ${SCRIPTS_PATH}
+echo '****************************'
+./mac.11.copy.static.libs.to.app.sh
+./mac.12.combine.static.libs.sh libvcxall
