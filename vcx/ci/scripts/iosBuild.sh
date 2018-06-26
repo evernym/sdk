04315373b2ea96bf3b5100a81238b181f46d6402
Ryan Marsh <ryan.marsh44@gmail.com>
2018-06-26 08:35:42 -0600

    fixed syntax in iosBuild3

--- vcx/ci/scripts/iosBuild.sh
+++ vcx/ci/scripts/iosBuild.sh
@@ -4,19 +4,17 @@ export SCRIPTS_PATH="vcx/libvcx/build_scripts/ios/mac"
 export WORK_DIR="/Users/jenkins"
 
 
-sh "ls"
-sh "cd ${SCRIPTS_PATH}"
-sh "ls"
-sh "${PWD}"
-sh "./mac.02.libindy.env.sh"
-sh "./mac.03.libindy.build.sh"
-sh "./mac.04.libvcx.setup.sh"
-sh "source ./mac.05.libvcx.env.sh"
-sh "./mac.06.libvcx.build.sh"
-
-sh "cp -rf ~/OpenSSL-for-iPhone ${WORK_DIR}/.macosbuild"
-sh "cp -rf ~/libzmq-ios ${WORK_DIR}/.macosbuild"
-sh "cp -rf ~/combine-libs ${WORK_DIR}/.macosbuild"
-
-sh "./mac.11.copy.static.libs.to.app.sh"
-sh "./mac.12.combine.static.libs.sh libvcxall"
+ls
+cd ${SCRIPTS_PATH}
+ls
+${PWD}
+./mac.02.libindy.env.sh
+./mac.03.libindy.build.sh
+./mac.04.libvcx.setup.sh
+source ./mac.05.libvcx.env.sh
+./mac.06.libvcx.build.sh
+cp -rf ~/OpenSSL-for-iPhone ${WORK_DIR}/.macosbuild
+cp -rf ~/libzmq-ios ${WORK_DIR}/.macosbuild
+cp -rf ~/combine-libs ${WORK_DIR}/.macosbuild
+./mac.11.copy.static.libs.to.app.sh
+./mac.12.combine.static.libs.sh libvcxall
