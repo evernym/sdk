87ee2305f8446fce9f3aeddb56ba4408027caf44
Ryan Marsh <ryan.marsh44@gmail.com>
2018-06-29 16:17:42 -0600

    working on parallel

--- vcx/ci/scripts/iosBuild.sh
+++ vcx/ci/scripts/iosBuild.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+export IOS_ARCH=$1
 export SCRIPTS_PATH="vcx/libvcx/build_scripts/ios/mac"
 export BASE_DIR="../../../../.."
 
 
 cd ${SCRIPTS_PATH}
-./mac.02.libindy.env.sh
-./mac.03.libindy.build.sh
 ./mac.04.libvcx.setup.sh
 source ./mac.05.libvcx.env.sh
-./mac.06.libvcx.build.sh
+./mac.06.libvcx.build.sh ${IOS_ARCH} 
 cp -rf ~/OpenSSL-for-iPhone ${BASE_DIR}/.macosbuild
 cp -rf ~/libzmq-ios ${BASE_DIR}/.macosbuild
 cp -rf ~/combine-libs ${BASE_DIR}/.macosbuild
