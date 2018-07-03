953602189014d6d416bde6f2e81b8f159a6ccdc3
Norman Jarvis <norman.jarvis@evernym.com>
2018-07-03 11:26:51 -0600

    [CO-1654] - Adding the wallet export/import APIs

--- vcx/wrappers/ios/vcx/vcx.framework/Headers/ConnectMeVcx.h
+++ vcx/wrappers/ios/vcx/vcx.framework/Headers/ConnectMeVcx.h
@@ -133,6 +133,12 @@ void VcxWrapperCommonNumberStringCallback(vcx_command_handle_t xcommand_handle,
     requestedPredicates:(NSString *)requestedPredicates
               proofName:(NSString *)proofName
              completion:(void (^)(NSError *error, NSString *proofHandle))completion;
+- (void)exportWallet:(NSString *)exportPath
+            offer:(NSString *)encryptionKey
+           completion:(void (^)(NSError *error, NSInteger exportHandle))completion;
+- (void)importWallet:(NSString *)exportPath
+            offer:(NSString *)encryptionKey
+           completion:(void (^)(NSError *error, NSInteger importHandle))completion;
 
 @end
 
