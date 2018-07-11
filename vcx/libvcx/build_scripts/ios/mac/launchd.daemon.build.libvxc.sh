3cb91187448c40ce8edb4870ebb7e8be18f7c05d
Norman Jarvis <norman.jarvis@evernym.com>
2018-07-11 12:06:27 -0600

    Turning off the debug symbols in the libvcx Cargo.toml file so that the libraries are smaller

--- vcx/libvcx/build_scripts/ios/mac/launchd.daemon.build.libvxc.sh
+++ vcx/libvcx/build_scripts/ios/mac/launchd.daemon.build.libvxc.sh
@@ -11,7 +11,7 @@ git pull > ./vcx/libvcx/build_scripts/ios/mac/git.pull.out 2>&1
 cd vcx/libvcx/build_scripts/ios/mac
 ./mac.03.libindy.build.sh > ./mac.03.libindy.build.sh.out 2>&1
 ./mac.04.libvcx.setup.sh > ./mac.04.libvcx.setup.sh.out 2>&1
-./mac.06.libvcx.build.sh > ./mac.06.libvcx.build.sh.out 2>&1
+./mac.06.libvcx.build.sh nodebug > ./mac.06.libvcx.build.sh.out 2>&1
 ./mac.11.copy.static.libs.to.app.sh > ./mac.11.copy.static.libs.to.app.sh.out 2>&1
 ./mac.12.combine.static.libs.sh libvcxall delete nodebug > ./mac.12.combine.static.libs.sh.out 2>&1
 ./mac.upload.ios.build.files.sh > ./mac.upload.ios.build.files.sh.out 2>&1
