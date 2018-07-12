54f2a4822709c9c3305f144587530379a62a6c04
Ryan Marsh <ryan.marsh44@gmail.com>
2018-07-12 20:27:32 +0530

    test android build fixes

--- vcx/ci/scripts/androidPackage.sh
+++ vcx/ci/scripts/androidPackage.sh
@@ -8,5 +8,13 @@ cp -v runtime_android_build/libvcx_x86/libvcx.so ${ANDROID_JNI_LIB}/x86
 cp -v runtime_android_build/libvcx_arm64/libvcx.so ${ANDROID_JNI_LIB}/arm64
 
 pushd vcx/wrappers/java/vcx
+    if [ -e local.properties ]; then
+       rm local.properties
+    fi
+cat <<EOT >> local.properties
+ndk.dir=/home/vcx/android-sdk-linux/ndk-bundle
+sdk.dir=/home/vcx/android-sdk-linux
+EOT
+
     ./gradlew clean assemble
 popd
