5ab258f07970f0013f4c2d0610c7cfa12c6bbf2c
Mark Hadley <mark.hadley@evernym.com>
2018-07-13 14:55:13 -0600

    Modifies publish

--- vcx/ci/scripts/publish.sh
+++ vcx/ci/scripts/publish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
-    echo "USAGE: $0 FILE LOOKUP_DIR TYPE CREDENTIALS"
-    exit 1
-fi
+#if [ $# -ne 4 ]; then
+#    echo "USAGE: $0 FILE LOOKUP_DIR TYPE CREDENTIALS"
+#    exit 1
+#fi
 
-FILENAME=$1
-LOOKUP_DIR=$2
-TYPE=$3
-CREDENTIALS=$4
+FILENAME="libvcx_0.1.16004266-a109a0c_amd64.deb"
+LOOKUP_DIR="/sdk/vcx/output"
+#TYPE=$3
+#CREDENTIALS=$4
 #URL="https://kraken.corp.evernym.com/repo/npm/upload"
 #URL="https://kraken.corp.evernym.com/repo/agency_dev/upload"
 URL="https://kraken.corp.evernym.com/repo/portal_dev/upload"
@@ -17,8 +17,7 @@ echo "Filename: ${FILENAME}"
 echo "TYPE: ${TYPE}"
 echo "Credentials: $(echo ${CREDENTIALS} | md5sum )"
 echo "URL: $URL"
-FILE=`find $LOOKUP_DIR -type f -name ${FILENAME}`
-ls $FILE
-curl -u ${CREDENTIALS} -X POST $URL $FILE
-echo result is "$?"
+
+FILE=`find $LOOKUP_DIR -type f -name $FILENAME`
+curl -u $KRAKEN_CREDENTIALS -X POST $URL $FILE
 
