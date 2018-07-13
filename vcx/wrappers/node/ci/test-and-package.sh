b46c8479f0e9a8273e5c598e8ade4a323152a569
Mark Hadley <mark.hadley@evernym.com>
2018-07-13 16:06:52 -0600

    Modified Test and Package script for Node

--- vcx/wrappers/node/ci/test-and-package.sh
+++ vcx/wrappers/node/ci/test-and-package.sh
@@ -4,8 +4,8 @@ npm run lint
 npm ci
 npm test
 npm pack
-rename \"s/\\.tgz\$/_amd64\\.tgz/\" *.tgz
-rename \"s/vcx-/vcx_/\" *.tgz
+rename \s/vcx-/vcx_/ *.tgz
+rename \s/\\.tgz\$/_amd64\\.tgz/ *.tgz
 cp *.tgz /sdk/vcx/output
 
 
