65bdd6cd744be68d4b6dd73ac5bed0de125346f2
Norman Jarvis <norman.jarvis@evernym.com>
2018-07-13 23:55:34 -0600

    Now the logic works to add in trace statements if the following line ends in a semicolon

--- vcx/libvcx/build_scripts/add.trace.statements.to.src.py
+++ vcx/libvcx/build_scripts/add.trace.statements.to.src.py
@@ -1,6 +1,7 @@
 #! /usr/local/bin/python3
 # findLargeFiles.py - given a folder name, walk through its entire hierarchy
 #                   - print folders and files within each folder
+# python add.trace.statements.to.src.py /Users/iosbuild1/forge/work/code/evernym/sdk/vcx/libvcx/src
 
 import os
 import sys
@@ -8,6 +9,7 @@ import sys
 # print(sys.argv)
 
 def recursive_walk(folder):
+    traceNumber = 0
     for folderName, subfolders, filenames in os.walk(folder):
         if subfolders:
             for subfolder in subfolders:
@@ -19,12 +21,16 @@ def recursive_walk(folder):
             print(folderName + '/' + filename + ".newrs")
             copy = open(folderName + '/' + filename + ".newrs", "w")
             for line in f:
+                trimmedLine = line.strip()
+                if trimmedLine.endswith(";"):
+                    traceNumber += 1
+                    copy.write("trace!(\"DEBUG TRACE FROM MOBILE TEAM -- " + str(traceNumber) + "\");\n")
                 copy.write(line)
             f.close()
             copy.close()
+            os.rename(folderName + '/' + filename + ".newrs", folderName + '/' + filename)
 
 recursive_walk(sys.argv[1])
-
 
 # find vcx/libvcx/src -name "*.rs"|wc -l
 
