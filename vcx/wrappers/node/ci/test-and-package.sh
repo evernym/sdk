fd04252cb399113138d24e0c7919a7221854f0c0
Mark Hadley <mark.hadley@evernym.com>
2018-07-16 15:10:22 -0600

    Testing Node Package Size

--- vcx/wrappers/node/ci/test-and-package.sh
+++ vcx/wrappers/node/ci/test-and-package.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 npm run lint
-npm run ci
-npm test
+npm ci
+#npm test
 npm pack
+echo "print working directory"
+pwd
+echo "directory listing"
+ls /sdk/vcx/output -al
+
+echo "Renaming artifact"
 rename \s/vcx-/vcx_/ *.tgz
 rename \s/\\.tgz\$/_amd64\\.tgz/ *.tgz
 cp *.tgz /sdk/vcx/output
