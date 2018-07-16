82d7f877b08dd9d7b1faf88dfc19d3ea2e73b3b7
Mark Hadley <mark.hadley@evernym.com>
2018-07-16 14:57:49 -0600

    Testing Node Package Size

--- vcx/wrappers/node/ci/test-and-package.sh
+++ vcx/wrappers/node/ci/test-and-package.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 npm run lint
-npm ci
+npm run ci
 npm test
 npm pack
 rename \s/vcx-/vcx_/ *.tgz
 rename \s/\\.tgz\$/_amd64\\.tgz/ *.tgz
 cp *.tgz /sdk/vcx/output
+ls /sdk/vcx/output -al
 
 
