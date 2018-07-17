f605cf345e56d744f09546b39497ec36755454ac
Mark Hadley <mark.hadley@evernym.com>
2018-07-17 14:23:37 -0600

    Cleanup of Files

--- vcx/ci/scripts/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-VOLUME_NAME=${1:-"data-vcx"} 
-docker volume rm -f $VOLUME_NAME
-
--- vcx/ci/scripts/create-debian.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build -f vcx/ci/Dockerfile-libindy -t libindy .
-docker build -f vcx/ci/Dockerfile-libvcx -t libvcx .
-docker run -u root --rm -v output:/output libvcx
