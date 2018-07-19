26323c61d5273d4893b5b3ae04de0e9e8929396d
Norman Jarvis <norman.jarvis@evernym.com>
2018-07-19 07:13:47 -0600

    Changed debuginfo to nodebug as the debuginfo symbols do not help it seems

--- vcx/libvcx/build_scripts/ios/mac/mac.norms.local.rust.dev.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.norms.local.rust.dev.sh
@@ -12,8 +12,8 @@ WORK_DIR=$(abspath "$WORK_DIR")
 #IOS_ARCHS="arm64,armv7,armv7s,x86_64"
 IOS_TARGETS="x86_64-apple-ios"
 IOS_ARCHS="x86_64"
-./mac.03.libindy.build.sh debuginfo "${IOS_TARGETS}" noclean > ./mac.norms.local.rust.dev.sh.out 2>&1
-./mac.06.libvcx.build.sh debuginfo "${IOS_TARGETS}" noclean >> ./mac.norms.local.rust.dev.sh.out 2>&1
+./mac.03.libindy.build.sh nodebug "${IOS_TARGETS}" noclean > ./mac.norms.local.rust.dev.sh.out 2>&1
+./mac.06.libvcx.build.sh nodebug "${IOS_TARGETS}" noclean >> ./mac.norms.local.rust.dev.sh.out 2>&1
 ./mac.11.copy.static.libs.to.app.sh >> ./mac.norms.local.rust.dev.sh.out 2>&1
-./mac.12.combine.static.libs.sh libvcxall delete debuginfo "${IOS_ARCHS}" > /dev/null 2>&1
+./mac.12.combine.static.libs.sh libvcxall delete nodebug "${IOS_ARCHS}" > /dev/null 2>&1
 ./mac.upload.ios.build.files.sh "${IOS_ARCHS}" >> ./mac.norms.local.rust.dev.sh.out 2>&1
