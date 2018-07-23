de371137e4b9c98f4712a79816e0d42fc8a8674c
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-07-23 16:20:03 +0530

    Modified jenkins to upload the .aar file to cloudrepo maven repository
    Signed-off-by: Mohammad Abdul Sami <faisal00813@gmail.com>

---
+++ vcx/ci/scripts/androidPublish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+AAR_FOLDER=vcx/wrappers/java/vcx/build/outputs/aar
+AAR_VERSION=$(find ${AAR_FOLDER} -type f -name 'com.evernym-vcx_*-release.aar'| perl -nle 'print $& if m{(?<=vcx_)(.*)(?=_x86)}' | head -1 | awk '{print $1}')
+echo "Uploading .aar with version number ==> ${AAR_VERSION}"
+cp settings.xml ${AAR_FOLDER}
+pushd ${AAR_FOLDER}
+
+    mvn -e deploy:deploy-file \
+        -Durl="https://evernym.mycloudrepo.io/repositories/libvcx-android" \
+        -DrepositoryId="io.cloudrepo" \
+        -Dversion=${AAR_VERSION} \
+        -Dfile="com.evernym-vcx_${AAR_VERSION}_x86-armv7-release.aar" \
+        -DartifactId="vcx" \
+        -Dpackaging="aar" \
+        -DgroupId="com.evernym"
+        --settings settings.xml
+popd
