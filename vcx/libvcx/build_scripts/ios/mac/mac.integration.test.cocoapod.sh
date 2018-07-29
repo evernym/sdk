daf7e05ad63bb7babe2a887f57bf7818a6e9322a
Norman Jarvis <norman.jarvis@evernym.com>
2018-07-28 21:56:27 -0600

    Added the build sdk to be iphonesimulator

--- vcx/libvcx/build_scripts/ios/mac/mac.integration.test.cocoapod.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.integration.test.cocoapod.sh
@@ -15,6 +15,6 @@ TEST_SCHEME=testlibvcxTests
 cd $TEST_WORKSPACE_ROOT
 pod install
 
-xcodebuild -workspace ${TEST_WORKSPACE} -scheme ${TEST_SCHEME} build-for-testing
+xcodebuild -workspace ${TEST_WORKSPACE} -scheme ${TEST_SCHEME} -sdk iphonesimulator build-for-testing
 ## Need to do: brew install xctool
 xctool -workspace ${TEST_WORKSPACE} -scheme ${TEST_SCHEME} run-tests -sdk iphonesimulator
