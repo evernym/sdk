601b1789d2f8b3c724a2b600deccbd996d8994ad
Ryan Marsh <ryan.marsh44@gmail.com>
2018-08-13 11:29:45 -0600

    libvcx android build in parallel without packaging

--- vcx/libvcx/build_scripts/ios/mac/mac.06.libvcx.build.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.06.libvcx.build.sh
@@ -33,10 +33,10 @@ if [ ! -z "$3" ]; then
     CLEAN_BUILD=$3
 fi
 
-#if [ "$CLEAN_BUILD" = "cleanbuild" ]; then
-    #cargo clean
-    # cargo update
-#fi
+if [ "$CLEAN_BUILD" = "cleanbuild" ]; then
+   cargo clean
+   # cargo update
+fi
 
 git log -1 > $WORK_DIR/evernym.vcx-sdk.git.commit.log
 
@@ -77,41 +77,14 @@ do
     export IOS_SODIUM_LIB=$WORK_DIR/libzmq-ios/libsodium-ios/dist/ios/lib/${target_arch}
     export IOS_ZMQ_LIB=$WORK_DIR/libzmq-ios/dist/ios/lib/${target_arch}
     export LIBINDY_DIR=$WORK_DIR/libindy/${target_arch}
-    #export LIBNULLPAY_DIR=$WORK_DIR/vcx-indy-sdk/libnullpay/target/${target}/release
     export LIBSOVTOKEN_DIR=$WORK_DIR/libsovtoken-ios/libsovtoken/${target}
 
-    # To build for macos
-    #cargo build
-    #export LIBINDY_DIR=/usr/local/lib
-    #export RUST_BACKTRACE=1
-    # To build for iOS
-    #LIBINDY_DIR=/usr/local/lib RUST_BACKTRACE=1 cargo lipo --release
-    #cargo lipo --release --verbose --targets="${IOS_TARGETS}"
-    
-    # if [ -f "./target/universal/release/libvcx.a" ]; then
-    #     mv ./target/universal/release/libvcx.a ./libvcx.previous.a
-    # fi
-
-    #rm ./target/universal/release/libvcx.a
-    #cargo lipo --release --verbose --targets="${target}"
     cargo build --release --no-default-features --features "ci sovtoken" --target "${target}"
-    # cargo build --release --target "${target}"
 
-    # if [ -f "./libvcx.previous.a" ]; then
-    #     lipo -create -output ./combined.ios.libvcx.a ./target/universal/release/libvcx.a ./libvcx.previous.a
-    #     mv ./combined.ios.libvcx.a ./target/universal/release/libvcx.a
-    #     rm ./libvcx.previous.a
-    # fi
     to_combine="${to_combine} ./target/${target}/release/libvcx.a"
 
 done
-#rm ./target/universal/release/libvcx.a
 mkdir -p ./target/universal/release
 lipo -create $to_combine -o ./target/universal/release/libvcx.a
-#lipo -create -output ./combined.ios.libvcx.a ./target/universal/release/libvcx.a ./libvcx.previous.a
 
 export OPENSSL_LIB_DIR=$OPENSSL_LIB_DIR_DARWIN
-
-#cargo test
-
-#lipo -info 
