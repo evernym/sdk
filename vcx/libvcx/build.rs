d600435fe46ed04758645a83860e78227c82f092
Mohammad Abdul Sami <faisal00813@gmail.com>
2018-08-14 13:27:34 +0530

    Build.rs revert

--- vcx/libvcx/build.rs
+++ vcx/libvcx/build.rs
@@ -87,13 +87,7 @@ fn main() {
         println!("cargo:rustc-link-lib=static=ssl");
         if cfg!(feature = "nullpay") {
             let libnullpay_lib_path = match env::var("LIBNULLPAY_DIR") {
-                Ok(val) => {
-                    if Path::new(format!("{}/lib",val)).exists() {
-                        return format!("{}/lib",val)
-                    }else{
-                        return val
-                    }
-                },
+                Ok(val) => val,
                 Err(..) => panic!("Missing required environment variable LIBNULLPAY_DIR")
             };
 
