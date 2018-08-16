1066f74e41a369f78dffb9b219bc59693dc6ed00
Ryan Marsh <ryan.marsh44@gmail.com>
2018-08-16 13:28:46 -0600

    test android package

--- vcx/ci/scripts/androidPackage.sh
+++ vcx/ci/scripts/androidPackage.sh
@@ -3,6 +3,10 @@
 set -e
 echo ${PWD}
 ANDROID_JNI_LIB=vcx/wrappers/java/vcx/src/main/jniLibs
+
+mkdir -p ${ANDROID_JNI_LIB}/arm
+mkdir -p ${ANDROID_JNI_LIB}/x86
+mkdir -p ${ANDROID_JNI_LIB}/arm64
 cp -v runtime_android_build/libvcx_arm/libvcx.so ${ANDROID_JNI_LIB}/arm
 cp -v runtime_android_build/libvcx_x86/libvcx.so ${ANDROID_JNI_LIB}/x86
 cp -v runtime_android_build/libvcx_arm64/libvcx.so ${ANDROID_JNI_LIB}/arm64
