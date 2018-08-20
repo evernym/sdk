4b2df206a00985d3d6d3f8eb92070f66e2fcc398
Norman Jarvis <norman.jarvis@evernym.com>
2018-08-20 12:44:24 -0600

    Adding some optimizations to the libindy build so that it is a smaller size for iOS

--- vcx/libvcx/build_scripts/ios/mac/mac.03.libindy.build.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.03.libindy.build.sh
@@ -56,9 +56,11 @@ if [ "$#" -gt 0 ]; then
     #########################################################################################################################
     cd $WORK_DIR/vcx-indy-sdk/libindy
 
-    #if [ "$DEBUG_SYMBOLS" = "debuginfo" ]; then
+    if [ "$DEBUG_SYMBOLS" = "debuginfo" ]; then
         cat $START_DIR/cargo.toml.add.debug.txt >> Cargo.toml
-    #fi
+    else
+        cat $START_DIR/cargo.toml.reduce.size.txt >> Cargo.toml
+    fi
     if [ "$DEBUG_SYMBOLS" = "nodebug" ]; then
         sed -i .bak 's/debug = true/debug = false/' Cargo.toml
     fi
