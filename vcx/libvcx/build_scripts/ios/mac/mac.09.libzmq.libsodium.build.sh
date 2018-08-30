6bf046d8ee3e76cbe78af78ab618629e84ff4e5b
Norman Jarvis <norman.jarvis@evernym.com>
2018-08-29 20:08:59 -0600

    Added back in the commented out parts of the script that checkout the code

--- vcx/libvcx/build_scripts/ios/mac/mac.09.libzmq.libsodium.build.sh
+++ vcx/libvcx/build_scripts/ios/mac/mac.09.libzmq.libsodium.build.sh
@@ -7,12 +7,12 @@ WORK_DIR=$START_DIR/../../../../../.macosbuild
 mkdir -p $WORK_DIR
 WORK_DIR=$(abspath "$WORK_DIR")
 
-# if [ -d $WORK_DIR/libzmq-ios ]; then
-#     rm -rf $WORK_DIR/libzmq-ios
-# fi
-# git clone https://github.com/evernym/libzmq-ios.git $WORK_DIR/libzmq-ios
-# cd $WORK_DIR/libzmq-ios
-# git clone https://github.com/evernym/libsodium-ios.git $WORK_DIR/libzmq-ios/libsodium-ios
+if [ -d $WORK_DIR/libzmq-ios ]; then
+    rm -rf $WORK_DIR/libzmq-ios
+fi
+git clone https://github.com/evernym/libzmq-ios.git $WORK_DIR/libzmq-ios
+cd $WORK_DIR/libzmq-ios
+git clone https://github.com/evernym/libsodium-ios.git $WORK_DIR/libzmq-ios/libsodium-ios
 cd $WORK_DIR/libzmq-ios/libsodium-ios
 ./libsodium.rb
 cd ..
