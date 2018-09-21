a4339b116ffe498383f77c8a0edc78c7858f7a8b
Ryan Marsh <ryan.marsh44@gmail.com>
2018-09-21 14:07:08 -0600

    Upgrades libvcx dependency from 0.9.2 to 0.9.3 of libsovtoken

--- vcx/ci/scripts/package.sh
+++ vcx/ci/scripts/package.sh
@@ -4,7 +4,7 @@ CURDIR=$(pwd)
 export PATH=${PATH}:$(pwd)/vcx/ci/scripts
 cd vcx/libvcx/
 cargo update-version
-# cargo test --no-default-features --features "ci sovtoken" -- --test-threads=1
+cargo test --no-default-features --features "ci sovtoken" -- --test-threads=1
 cargo build --no-default-features --features "ci sovtoken"
 cargo update-so
 cargo deb --no-build
