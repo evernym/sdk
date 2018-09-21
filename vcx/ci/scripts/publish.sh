24cef756c5b68739bd1f5953bcfc27f39aa2111f
Mark Hadley <mark.hadley@evernym.com>
2018-09-21 10:48:49 -0600

    Changes Jenkinsfile to Hide Linux Publishing Password

--- vcx/ci/scripts/publish.sh
+++ vcx/ci/scripts/publish.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# if [ $# -ne 2 ]; then
-#     echo "USAGE: $0 CREDENTIALS FILE URL"
-#     exit 1
-# fi
-
-CREDENTIALS=$KRAKEN_CREDENTIALS
 FILENAME=$1
 URL=$2
 
 echo "Filename: ${FILENAME}"
 echo "TYPE: ${TYPE}"
 echo "URL: $URL"
-echo "LOOKUP_DIR: $LOOKUP_DIR"
 
 echo 'info:'
 pwd
 ls -al
+echo "========="
+echo ${env.KRAKEN_CREDENTIALS}
+echo "========="
+echo "${env.KRAKEN_CREDENTIALS}"
+echo "========="
+echo '${env.KRAKEN_CREDENTIALS}'
+echo "========="
 echo 'end info'
 
-find $LOOKUP_DIR -type f -name ${FILENAME} -exec curl -u $CREDENTIALS -X POST $URL -F 'file=@{}' \;
+find ${env.LOOKUP_DIR} -type f -name ${FILENAME} -exec curl -u ${env.KRAKEN_CREDENTIALS} -X POST $URL -F 'file=@{}' \;
 
