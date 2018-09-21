1bd5ac220df574d02434d76aadafe349b234b47d
Mark Hadley <mark.hadley@evernym.com>
2018-09-21 13:40:47 -0600

    Modifies Jenkinsfile

--- vcx/ci/scripts/publish.sh
+++ vcx/ci/scripts/publish.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-set -e
 
-FILENAME=$1
-URL=$2
-LOOKUP_DIR='./output'
+if [ $# -ne 3 ]; then
+    echo "USAGE: $0 CREDENTIALS FILE URL"
+    exit 1
+fi
+
+CREDENTIALS=$1
+FILENAME=$2
+URL=$3
+LOOKUP_DIR="output"
 
 echo "Filename: ${FILENAME}"
 echo "TYPE: ${TYPE}"
@@ -12,14 +17,8 @@ echo "URL: $URL"
 echo 'info:'
 pwd
 ls -al
-echo "========="
-echo ${env.KRAKEN_CREDENTIALS}
-echo "========="
-echo "${env.KRAKEN_CREDENTIALS}"
-echo "========="
-echo '${env.KRAKEN_CREDENTIALS}'
-echo "========="
 echo 'end info'
 
-find ${LOOKUP_DIR} -type f -name ${FILENAME} -exec curl -u ${env.KRAKEN_CREDENTIALS} -X POST $URL -F 'file=@{}' \;
+find "./output" -type f -name ${FILENAME} -exec curl -u $CREDENTIALS -X POST $URL -F 'file=@{}' \;
+
 
