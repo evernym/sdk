b73dbf0c70a7f728e5f6d32c0bf35110102f8367
Mark Hadley <mark.hadley@evernym.com>
2018-09-26 17:19:33 -0600

    Fixes Node package.sh

--- vcx/wrappers/node/ci/package.sh
+++ vcx/wrappers/node/ci/package.sh
@@ -7,10 +7,10 @@ npm i
 npm run compile
 npm pack
 
-rename \s/vcx-/vcx_/ *.tgz
+rename \s/node-vcx-wrapper-/node-vcx-wrapper_/ *.tgz
 rename \s/\\.tgz\$/_amd64\\.tgz/ *.tgz
 
-find . -type f -name 'node-vcx*.tgz' -exec create_npm_deb.py {} \;
+find . -type f -name 'node-vcx-wrapper*.tgz' -exec create_npm_deb.py {} \;
 
 cd $CURDIR
 cp $DIR/node-vcx*.tgz $OUTPUTDIR
