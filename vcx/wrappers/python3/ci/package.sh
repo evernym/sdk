dbb0260bf39376c89c58b7a8d4ac9da157402f44
Mark Hadley <mark.hadley@evernym.com>
2018-09-26 16:01:44 -0600

    Modifies the Name Of the Python Package to Have an Underscore

--- vcx/wrappers/python3/ci/package.sh
+++ vcx/wrappers/python3/ci/package.sh
@@ -6,11 +6,12 @@ export DIST=`pwd`/vcx/wrappers/python3/dist/
 export PACKAGE_NAME='python3-vcx-wrapper'
 pushd vcx/wrappers/python3
 python3 setup.py sdist
-
+PACKAGE=${PACKAGE_NAME}-${VCX_VERSION}.tar.gz
+FILELY_PACKAGE=${PACKAGE_NAME}_${VCX_VERSION}.tar.gz
 # Added test so that we can confirm the new package name and that it was created.
-if [ `ls ${DIST}/${PACKAGE_NAME}*.tar.gz | wc -l` == "0" ]; then
+if [ ! -e ${DIST}/${PACKAGE} ]; then
     echo "Python Package Not Created"
     exit 1
 fi
 popd
-cp ${DIST}/${PACKAGE_NAME}*.tar.gz output
+mv ${DIST}/${PACKAGE} output/${FILELY_PACKAGE}
