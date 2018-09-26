11ef79aff8d433ec505edbe1705ebe78dc2246d0
Mark Hadley <mark.hadley@evernym.com>
2018-09-26 15:17:53 -0600

    Renames Python Package

--- vcx/wrappers/python3/ci/package.sh
+++ vcx/wrappers/python3/ci/package.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+set -e
 export PATH=${PATH}:$(pwd)/vcx/ci/scripts
 export VCX_VERSION=$(toml_utils.py vcx/libvcx/Cargo.toml)
+export DIST=`pwd`/vcx/wrappers/python3/dist/
+export PACKAGE_NAME='python3-vcx-wrapper'
 pushd vcx/wrappers/python3
 python3 setup.py sdist
+
+# Added test so that we can confirm the new package name and that it was created.
+if [ `ls ${DIST}/${PACKAGE_NAME}*.tar.gz | wc -l` == "0" ]; then
+    echo "Python Package Not Created"
+    exit 1
+fi
 popd
-cp vcx/wrappers/python3/dist/*.tar.gz output
+cp ${DIST}/${PACKAGE_NAME}*.tar.gz output
